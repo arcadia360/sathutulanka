df0526172eeb9a8261007d71012d2e4ae38b54ec
Viraj Lasitha <47885958+VirajLasitha@users.noreply.github.com>
2021-03-10 17:54:02 +0530

    Update Registration.php

--- application/controllers/Registration.php
+++ application/controllers/Registration.php
@@ -3,24 +3,30 @@ class Registration extends CI_Controller
 {
 	public function index()
 	{
-		$this->load->view('user_registration/header');
-		$this->load->view('user_registration/sample');
-		$this->load->view('user_registration/footer');
+		$this->load->view('registration/header');
+		$this->load->view('registration/sample');
+		$this->load->view('registration/footer');
+	}
+
+	public function CreateAccount(){
+		// $this->load->view('registration/header');
+		$this->load->view('registration/create_account');
+		// $this->load->view('registration/footer');
 	}
 
 	public function addPhysicalStatus()
 	{
-		$this->load->view('user_registration/header');
-		$this->load->view('user_registration/physical_status');
-		$this->load->view('user_registration/footer');
+		$this->load->view('registration/header');
+		$this->load->view('registration/physical_status');
+		$this->load->view('registration/footer');
 	}
 	public function langTest()
 	{
 		$this->load->helper('language');
 		$this->lang->load('si', 'Sinhala');
 		echo lang('hello');
-		$this->load->view('user_registration/header');
-		$this->load->view('user_registration/physical_status');
-		$this->load->view('user_registration/footer');
+		$this->load->view('registration/header');
+		$this->load->view('registration/physical_status');
+		$this->load->view('registration/footer');
 	}
 }
