331dfe7f15090e2fdca301de35722ddbddbf61ac
Viraj Lasitha <47885958+VirajLasitha@users.noreply.github.com>
2021-03-10 17:53:56 +0530

    Update create_account.php

---
+++ application/views/registration/create_account.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sathutu Lanka | User Registration</title>
+
+	<link rel="stylesheet" href="<?php echo base_url('resources/css/registration_form/style.css') ?>">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+    <!-- <div class="container"> -->
+        <div class="row">
+            <div class="col-lg-8 col-md-7 d-none d-md-block d-lg-block" style="background-color: blue;">
+            A
+            </div>
+            <div class="col-lg-4 col-md-5 col-sm-12" style="background-color: yellow;">
+            B
+            </div>
+        </div>
+    <!-- </div> -->
+    
+</body>
+</html>
