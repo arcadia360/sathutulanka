762f73eb7ce942452b9d3c49906150a8866b55f8
dhanu159 <randimakumanayake@yahoo.com>
2021-03-11 09:41:26 +0530

    Update en_lang.php

--- application/language/english/en_lang.php
+++ application/language/english/en_lang.php
@@ -1,4 +1,16 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['hello'] = 'hello';
+// Physical Status
+$lang['physicalStatus'] = 'Physical Status';
+$lang['height'] = 'Height';
+$lang['weight'] = 'Weight';
+
+// Background
+$lang['background'] = 'Background';
+$lang['motherTongue'] = 'Mother Toungue';
+$lang['ethnicity'] = 'Ethnicity';
+$lang['religion'] = 'Religion';
+$lang['caste'] = 'Caste';
+$lang['subCast'] = 'Sub Cast';
+$lang['policeReport'] = 'Police Report';
