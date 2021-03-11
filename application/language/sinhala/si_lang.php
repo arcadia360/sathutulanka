09f92acda60ce2624ac1aa5a778a8dd2ce925f33
dhanu159 <randimakumanayake@yahoo.com>
2021-03-11 09:41:21 +0530

    Update si_lang.php

--- application/language/sinhala/si_lang.php
+++ application/language/sinhala/si_lang.php
@@ -1,4 +1,17 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['hello'] = 'හෙලෝ';
+// Physical Status
+$lang['physicalStatus'] = 'භෞතික තත්වය';
+$lang['height'] = 'උස';
+$lang['weight'] = 'බර';
+
+
+// Background
+$lang['background'] = 'පසුබිම';
+$lang['motherTongue'] = 'මවු භාෂාව';
+$lang['ethnicity'] = 'ජනවාර්ගිකත්වය';
+$lang['religion'] = 'ආගම';
+$lang['caste'] = 'කුලය';
+$lang['subCast'] = 'උප කුලය';
+$lang['policeReport'] = 'පොලිස් වාර්තාව';
