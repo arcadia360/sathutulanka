87a5f458321882237fa4b603922e924081fff195
dhanu159 <randimakumanayake@yahoo.com>
2021-03-12 00:17:40 +0530

    Create Model_registration.php

---
+++ application/models/Model_registration.php
@@ -0,0 +1,32 @@
+<?php
+class Model_registration extends CI_Model
+{
+  public function savePhysicalStatus()
+  {
+    $weight = $this->input->post('weight');
+    // arrange from weight and to wight
+    $weightsArray = explode("-", $weight);
+    $fromWeight = $weightsArray[0];
+    $toWeight = $weightsArray[1];
+
+    $id = 1;
+
+    $data = array(
+      'intHeight' => $this->input->post('height'),
+      'intWeightFrom' => $fromWeight,
+      'intWeightTo' => $toWeight,
+      'intBodyTypeID' => $this->input->post('bodyShape'),
+      'intSkinColourID' => $this->input->post('skinColor'),
+      'isDisability' => $this->input->post('disability'),
+      'vcBloodGroup' => $this->input->post('bloodGroup'),
+      'isHealthInfo' => $this->input->post('healthInfo')
+    );
+    $this->db->where('intUserID', $id);
+    $this->db->update('user', $data);
+    if ($this->db->affected_rows() > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
