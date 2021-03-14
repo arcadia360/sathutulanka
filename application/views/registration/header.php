dec8bf52f6c1d956a9524c483a03d37db5544fc4
dhanu159 <randimakumanayake@yahoo.com>
2021-03-14 21:28:20 +0530

    Update header.php

--- application/views/registration/header.php
+++ application/views/registration/header.php
@@ -7,11 +7,13 @@
 	<link rel="stylesheet" href="<?php echo base_url('resources/css/registration_form/style.css') ?>">
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 	<!-- DK CSS -->
 	<link rel="stylesheet" href="<?= base_url('resources/css/common_dk.css') ?>">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
 	<style>
 		.main-section {
