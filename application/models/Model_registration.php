2790f89707b56aba637bd42612002a1f749daa78
Ramodk <42975445+Ramodk@users.noreply.github.com>
2021-03-26 01:49:30 +0530

    Update Model_registration.php

--- application/models/Model_registration.php
+++ application/models/Model_registration.php
@@ -4,7 +4,7 @@ class Model_registration extends CI_Model
 
   public function __construct()
   {
-      parent::__construct();
+    parent::__construct();
   }
 
   public function savePhysicalStatus()
@@ -51,20 +51,69 @@ class Model_registration extends CI_Model
   {
     if ($language_id == 1) { // English
       $sql = "SELECT  intProvidingInformationTypeID, vcProvidingInformationType_en AS vcProvidingInformationType FROM providinginformationtype";
-        $query = $this->db->query($sql);
-        return $query->result_array();
-
-		} else if ($language_id == 2) { // Sinhala
-      $sql = "SELECT  intProvidingInformationTypeID, vcProvidingInformationType_si AS vcProvidingInformationType FROM providinginformationtype;";
       $query = $this->db->query($sql);
       return $query->result_array();
-      
-		} else if ($language_id == 3) { // Tamil
-      $sql = "SELECT  intProvidingInformationTypeID, vcProvidingInformationType_ta AS vcProvidingInformationType FROM providinginformationtype;";
+    } else if ($language_id == 2) { // Sinhala
+      $sql = "SELECT  intProvidingInformationTypeID, vcProvidingInformationType_si AS vcProvidingInformationType FROM providinginformationtype";
       $query = $this->db->query($sql);
       return $query->result_array();
-      
-		}
+    } else if ($language_id == 3) { // Tamil
+      $sql = "SELECT  intProvidingInformationTypeID, vcProvidingInformationType_ta AS vcProvidingInformationType FROM providinginformationtype";
+      $query = $this->db->query($sql);
+      return $query->result_array();
+    }
   }
+
+  public function getMaritalStatus($language_id)
+  {
+    if ($language_id == 1) { // English
+      $sql = "SELECT intMaritalStatusID, vcMaritalStatus_en AS vcMaritalStatus FROM maritalstatus";
+      $query = $this->db->query($sql);
+      return $query->result_array();
+    } else if ($language_id == 2) { // Sinhala
+      $sql = "SELECT intMaritalStatusID, vcMaritalStatus_si AS vcMaritalStatus FROM maritalstatus";
+      $query = $this->db->query($sql);
+      return $query->result_array();
+    } else if ($language_id == 3) { // Tamil
+      $sql = "SELECT intMaritalStatusID, vcMaritalStatus_ta AS vcMaritalStatus FROM maritalstatus";
+      $query = $this->db->query($sql);
+      return $query->result_array();
+    }
+  }
+
+  public function getMarriageType($language_id)
+  {
+    if ($language_id == 1) { // English
+      $sql = "SELECT intMarriageTypeID, vcMarriageType_en AS vcMarriageType FROM marriagetype";
+      $query = $this->db->query($sql);
+      return $query->result_array();
+    } else if ($language_id == 2) { // Sinhala
+      $sql = "SELECT intMarriageTypeID, vcMarriageType_si AS vcMarriageType FROM marriagetype";
+      $query = $this->db->query($sql);
+      return $query->result_array();
+    } else if ($language_id == 3) { // Tamil
+      $sql = "SELECT intMarriageTypeID, vcMarriageType_ta AS vcMarriageType FROM marriagetype";
+      $query = $this->db->query($sql);
+      return $query->result_array();
+    }
+  }
+
+  public function getNoofChildren($language_id)
+  {
+    if ($language_id == 1) { // English
+      $sql = "SELECT intNoOfChildrenID, vcNoOfChildren_en AS vcNoOfChildren FROM noofchildren";
+      $query = $this->db->query($sql);
+      return $query->result_array();
+    } else if ($language_id == 2) { // Sinhala
+      $sql = "SELECT intNoOfChildrenID, vcNoOfChildren_si AS vcNoOfChildren FROM noofchildren";
+      $query = $this->db->query($sql);
+      return $query->result_array();
+    } else if ($language_id == 3) { // Tamil
+      $sql = "SELECT intNoOfChildrenID, vcNoOfChildren_ta AS vcNoOfChildren FROM noofchildren";
+      $query = $this->db->query($sql);
+      return $query->result_array();
+    }
+  }
+
 
 }
