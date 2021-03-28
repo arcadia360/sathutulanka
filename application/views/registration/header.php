f2951c4e4146512e3868e64c00303c3bc9f405a2
Viraj <virajlasithazeero@gmail.com>
2021-03-28 21:32:01 +0530

    Update header.php

--- application/views/registration/header.php
+++ application/views/registration/header.php
@@ -4,12 +4,22 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Sathutu Lanka | User Registration</title>
+
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-	<link rel="stylesheet" href="<?php echo base_url('resources/css/registration_form/style.css') ?>">
+	<!-- Bootstrap 4.0 -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<!-- Common CSS -->
+	<link rel="stylesheet" href="<?= base_url('resources/css/common.css') ?>">
+	<!-- Navbar CSS -->
+	<link rel="stylesheet" href="<?= base_url('resources/css/navbar.css') ?>">
+	<!-- Animate CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" </head>
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+
+	<link rel="stylesheet" href="<?php echo base_url('resources/css/registration_form/style.css') ?>">
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"> -->
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css"> -->
 	<!-- DK CSS -->
 	<link rel="stylesheet" href="<?= base_url('resources/css/common_dk.css') ?>">
 
@@ -43,3 +53,40 @@
 </head>
 
 <body>
+
+	<nav class="navbar navbar-expand-lg bg-red">
+
+		<a class="navbar-brand" href="#">
+			<?php
+			if ($_SESSION['language_id'] == 1) { // Current Language - English
+			?>
+				<img class="img-fluid navbar-logo" src="<?= base_url('resources/images/navbar-logo-en.png') ?>" alt="Sathutu Lanka">
+			<?php
+			} else if ($_SESSION['language_id'] == 2) { // Current Language - Sinhala
+			?>
+				<img class="img-fluid navbar-logo" src="<?= base_url('resources/images/navbar-logo-si.png') ?>" alt="සතුටු ලංකා">
+			<?php
+			}
+			?>
+
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+			<i class="fas fa-bars"></i>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+			<ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+				<!-- <li class="nav-item active">
+                    <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Disabled</a>
+                </li> -->
+			</ul>
+
+			<a class="nav-link btn-lang" href="">සිංහල</a>
+
+		</div>
+	</nav>
