9ea951d9e3b60f7bd589d15fa15c44e0fba218bd
dhanu159 <randimakumanayake@yahoo.com>
2021-03-28 17:06:21 +0530

    Update background.php

---
+++ application/views/registration/background.php
@@ -0,0 +1,139 @@
+<canvas id="sakura"></canvas>
+<div class="btnbg" style="padding-top: 30px;">
+  <!-- Container Area Start -->
+  <div class="container" style="z-index: 1;">
+    <div class="row">
+      <div class="offset-lg-2 col-lg-8 col-md-12 col-sm-12 border rounded main-section">
+        <h3 class="text-center text-inverse"><?= lang('background') ?></h3>
+        <hr>
+        <form class="container" id="needs-validation" novalidate>
+          <div class="row">
+            <div class="col-12">
+              <div class="form-group">
+                <label class="text-inverse" for="validationCustom01"><?= lang('motherTongue') ?></label>
+                <select class="custom-select d-block form-control" id="motherTongue" name="motherTongue">
+                  <option value="0"><?= lang('select') ?></option>
+                  <option value="sinhala"><?= lang('sinhala') ?></option>
+                  <option value="tamil"><?= lang('tamil') ?></option>
+                  <option value="english"><?= lang('english') ?></option>
+                  <option value="other"><?= lang('other') ?></option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-12">
+              <div class="form-group">
+                <label class="text-inverse" for="validationCustom01"><?= lang('ethnicity') ?></label>
+                <select class="custom-select d-block form-control" id="ethnicity" name="ethnicity">
+                  <option value="0"><?= lang('select') ?></option>
+                  <option value="Sinhalese"><?= lang('Sinhalese') ?></option>
+                  <option value="sriLankanTamils"><?= lang('sriLankanTamils') ?></option>
+                  <option value="muslims"><?= lang('muslims') ?></option>
+                  <option value="indianTamils"><?= lang('indianTamils') ?></option>
+                  <option value="sriLankanMalays"><?= lang('sriLankanMalays') ?></option>
+                  <option value="burghers"><?= lang('burghers') ?></option>
+                  <option value="mentionedInMyStory"><?= lang('mentionedInMyStory') ?></option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-12">
+              <div class="form-group">
+                <label class="text-inverse" for="validationCustom01"><?= lang('religion') ?></label>
+                <select class="custom-select d-block form-control" id="religion" name="religion">
+                  <option value="0"><?= lang('select') ?></option>
+                  <option value="buddhist"><?= lang('buddhist') ?></option>
+                  <option value="hindu"><?= lang('hindu') ?></option>
+                  <option value="islam"><?= lang('islam') ?></option>
+                  <option value="christians"><?= lang('christians') ?></option>
+                  <option value="romanCatholic"><?= lang('romanCatholic') ?></option>
+                  <option value="mentionedInMyStory"><?= lang('mentionedInMyStory') ?></option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-12">
+              <div class="form-group">
+                <label class="text-inverse" for="validationCustom01"><?= lang('caste') ?></label>
+                <select class="custom-select d-block form-control" id="caste" name="caste">
+                  <option value="0"><?= lang('select') ?></option>
+                  <option value="">Select 1</option>
+                  <option value="">Select 2</option>
+                  <option value="mentionedInMyStory"><?= lang('mentionedInMyStory') ?></option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-12">
+              <div class="form-group">
+                <label class="text-inverse" for="validationCustom01"><?= lang('subCast') ?></label>
+                <select class="custom-select d-block form-control" id="subCast" name="subCast">
+                  <option value="0"><?= lang('select') ?></option>
+                  <option value="">Select 1</option>
+                  <option value="">Select 2</option>
+                  <option value="mentionedInMyStory"><?= lang('mentionedInMyStory') ?></option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="row">
+            <div class="col-12">
+              <label class="text-inverse" for="validationCustom01"><?= lang('policeReport') ?></label>
+              <div class="row">
+                <div class="col-6">
+                  <label class="custom-control custom-radio">
+                    <input id="bodyShape" name="bodyShape" type="radio" class="custom-control-input" value="1">
+                    <span class="custom-control-indicator"></span>
+                    <span class="custom-control-description"><?= lang('canProvide') ?></span>
+                  </label>
+                </div>
+                <div class="col-6">
+                  <label class="custom-control custom-radio">
+                    <input id="bodyShape" name="bodyShape" type="radio" class="custom-control-input" value="1">
+                    <span class="custom-control-indicator"></span>
+                    <span class="custom-control-description"><?= lang('cantProvide') ?></span>
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <hr>
+          <div class="row">
+            <div class="col-lg-6 col-sm-6 col-6 text-center" style="padding: 10px;">
+              <button class="btn btn-info" style="width: 150px;" id="btnBack" type="button">Back</button>
+            </div>
+            <div class="col-lg-6 col-sm-6 col-6 text-center" style="padding: 10px;">
+              <button class="btn btn-info" style="width: 150px;" type="button" id="btnSubmit">CONTINUE</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <!-- Container Area End -->
+</div>
+
+<script>
+  $(function() {
+
+    $('#btnBack').click(function() {
+      window.location.href = "<?php echo base_url('Registration/residence') ?>";
+    });
+
+    $('#btnSubmit').click(function() {
+      window.location.href = "<?php echo base_url('Registration/lifeStyle') ?>";
+    });
+
+  });
+</script>
