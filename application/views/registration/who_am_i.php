8a95c436023aee123a6230b1dfcfcebbc28f4ff8
Viraj <virajlasithazeero@gmail.com>
2021-03-28 21:04:07 +0530

    Update who_am_i.php

--- application/views/registration/who_am_i.php
+++ application/views/registration/who_am_i.php
@@ -1,5 +1,5 @@
 <style>
-.slidecontainer {
+    .slidecontainer {
         width: 100%;
     }
 
@@ -42,7 +42,7 @@
         <div class="row">
             <div class="offset-lg-2 col-lg-8 col-md-12 col-sm-12 border rounded main-section">
                 <h3 class="text-center text-inverse title">Who Am I</h3>
-                <hr>
+                <button class="btn btn-info" style="width: 150px; margin-bottom:20px; float:right;" type="button" id="btnSubmit">Skip this step &nbsp; <i class="fas fa-forward"></i></button>
                 <form class="container" id="needs-validation" novalidate>
                     <div class="row">
                         <div class="col-12">
