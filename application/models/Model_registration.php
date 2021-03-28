eef90ab2aed8f6439070fd3e5942ba5471a05ef0
dhanu159 <randimakumanayake@yahoo.com>
2021-03-28 17:05:59 +0530

    Update Model_registration.php

--- application/models/Model_registration.php
+++ application/models/Model_registration.php
@@ -37,23 +37,6 @@ class Model_registration extends CI_Model
     }
   }
 
-  public function getProvidingInformationType($language_id)
-  {
-    if ($language_id == 1) { // English
-      $sql = "SELECT  intProvidingInformationTypeID, vcProvidingInformationType_en AS vcProvidingInformationType FROM providinginformationtype";
-      $query = $this->db->query($sql);
-      return $query->result_array();
-    } else if ($language_id == 2) { // Sinhala
-      $sql = "SELECT  intProvidingInformationTypeID, vcProvidingInformationType_si AS vcProvidingInformationType FROM providinginformationtype";
-      $query = $this->db->query($sql);
-      return $query->result_array();
-    } else if ($language_id == 3) { // Tamil
-      $sql = "SELECT  intProvidingInformationTypeID, vcProvidingInformationType_ta AS vcProvidingInformationType FROM providinginformationtype";
-      $query = $this->db->query($sql);
-      return $query->result_array();
-    }
-  }
-
   public function loadCountries()
   {
     $query = $this->db->query("SELECT * FROM country");
@@ -131,6 +114,4 @@ class Model_registration extends CI_Model
       return $query->result_array();
     }
   }
-
-
 }
