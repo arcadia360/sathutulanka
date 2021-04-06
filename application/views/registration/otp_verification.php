0c1d3db4e2e736d68bcdf7bac2e937e948184fe2
Viraj Lasitha <47885958+VirajLasitha@users.noreply.github.com>
2021-04-06 18:20:48 +0530

    @ ofz

--- application/views/registration/otp_verification.php
+++ application/views/registration/otp_verification.php
@@ -1,41 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
+<style>
+    h3 {
+        font-size: 1.5em;
+    }
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OTP Verification</title>
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-    <!-- Bootstrap 4.0 -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <!-- Common CSS -->
-    <link rel="stylesheet" href="<?= base_url('resources/css/common.css') ?>">
-    <!-- Navbar CSS -->
-    <link rel="stylesheet" href="<?= base_url('resources/css/navbar.css') ?>">
-</head>
+    input[type=text] {
+        outline: 1px solid #b52b4a;
+        border: 1px solid transparent;
+    }
 
-<body>
-    <div id="wrapper">
-        <div id="dialog">
-            <button class="close">×</button>
-            <h3>Please enter the 4-digit verification code we sent via SMS:</h3>
-            <span>(we want to make sure it's you before we contact our movers)</span>
-            <div id="form">
-                <input type="text" maxLength="1" size="1" min="0" max="9" pattern="[0-9]{1}" />
-                <input type="text" maxLength="1" size="1" min="0" max="9" pattern="[0-9]{1}" /><input type="text" maxLength="1" size="1" min="0" max="9" pattern="[0-9]{1}" /><input type="text" maxLength="1" size="1" min="0" max="9" pattern="[0-9]{1}" />
-                <button class="btn btn-primary btn-embossed">Verify</button>
+    input[type=text]:focus {
+        border-color: #b52b4a;
+        box-shadow: 0 0 5px #b52b4a inset;
+        border-radius: 0;
+    }
+</style>
+<div class="container-fluid" style="background-color: white; height:500px;">
+
+    <div class="col-md-6 offset-md-3 col-sm-12 text-center" style="background-color: white; margin-top: 20px;">
+        <div class="row">
+            <div class="col">
+                <h3>Please enter the 4-digit verification <br> code we sent via SMS:</h3>
+                <span>(we want to make sure it's you before we contact our users)</span>
             </div>
-
-            <div>
-                Didn't receive the code?<br />
-                <a href="#">Send code again</a><br />
-                <a href="#">Change phone number</a>
+        </div>
+        <div class="row">
+            <div style="margin: 20px auto;">
+                <div style="width: 100px; padding-left: 10px; padding-right: 10px; position: relative; float:left;">
+                    <input class="form-control form-control-lg text-center" type="text" placeholder="_" maxlength="1">
+                </div>
+                <div style="width: 100px; padding-left: 10px; padding-right: 10px;  position: relative; float:left;">
+                    <input class="form-control form-control-lg text-center" type="text" placeholder="_" maxlength="1">
+                </div>
+                <div style="width: 100px; padding-left: 10px; padding-right: 10px;  position: relative; float:left;">
+                    <input class="form-control form-control-lg text-center" type="text" placeholder="_" maxlength="1">
+                </div>
+                <div style="width: 100px; padding-left: 10px; padding-right: 10px;  position: relative; float:left;">
+                    <input class="form-control form-control-lg text-center" type="text" placeholder="_" maxlength="1">
+                </div>
             </div>
-            <img src="http://jira.moovooz.com/secure/attachment/10424/VmVyaWZpY2F0aW9uLnN2Zw==" alt="test" />
+        </div>
+        <div class="row">
+            <button type="button" style="margin: 0 auto; width:200px; margin-top: 20px;" class="btn btn-success btn-lg">VERIFY</button>
+        </div>
+        <div class="row">
+            <p style="width: 100%; text-align: center; margin-top: 30px;">Didn't receive the code?</p>
+            <a href="" style="width: 100%; text-align: center; margin-top: 30px;">Send code again</a>
+            <a href="" style="width: 100%; text-align: center;">Change phone number</a>
         </div>
     </div>
-</body>
-
-</html>
+</div>
+</div>
