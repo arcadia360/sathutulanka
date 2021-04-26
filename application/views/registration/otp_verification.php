f9d8b72d2b391a9aa0a1d26f1e88989ea00e4ad6
Ramodk <42975445+Ramodk@users.noreply.github.com>
2021-04-26 11:35:40 +0530

    Update otp_verification.php

--- application/views/registration/otp_verification.php
+++ application/views/registration/otp_verification.php
@@ -1,7 +1,6 @@
-
 <head>
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
-  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
 </head>
 
 <style>
@@ -35,27 +34,27 @@
         <div class="row">
             <div style="margin: 20px auto;">
                 <div style="width: 100px; padding-left: 10px; padding-right: 10px; position: relative; float:left;">
-                    <input class="form-control form-control-lg text-center" type="text" placeholder="_" maxlength="1">
+                    <input class="form-control form-control-lg text-center only-decimal" type="text" id="no1" placeholder="_" maxlength="1">
                 </div>
                 <div style="width: 100px; padding-left: 10px; padding-right: 10px;  position: relative; float:left;">
-                    <input class="form-control form-control-lg text-center" type="text" placeholder="_" maxlength="1">
+                    <input class="form-control form-control-lg text-center only-decimal" type="text" id="no2" placeholder="_" maxlength="1">
                 </div>
                 <div style="width: 100px; padding-left: 10px; padding-right: 10px;  position: relative; float:left;">
-                    <input class="form-control form-control-lg text-center" type="text" placeholder="_" maxlength="1">
+                    <input class="form-control form-control-lg text-center only-decimal" type="text" id="no3" placeholder="_" maxlength="1">
                 </div>
                 <div style="width: 100px; padding-left: 10px; padding-right: 10px;  position: relative; float:left;">
-                    <input class="form-control form-control-lg text-center" type="text" placeholder="_" maxlength="1">
+                    <input class="form-control form-control-lg text-center only-decimal" type="text" id="no4" placeholder="_" maxlength="1">
                 </div>
             </div>
         </div>
         <div class="row">
-            <button type="button" style="margin: 0 auto; width:200px; margin-top: 20px;" class="btn btn-success btn-lg">VERIFY</button>
+            <button type="button" id="btnVerify" style="margin: 0 auto; width:200px; margin-top: 20px;" class="btn btn-success btn-lg">VERIFY</button>
         </div>
         <div class="row">
             <p style="width: 100%; text-align: center; margin-top: 30px;">Didn't receive the code?</p>
             <input type="hidden" id="EmailVerificationCode" name="EmailVerificationCode" value=<?php echo $verificationText; ?>>
             <a href="#" id="otpResend" style="width: 100%; text-align: center; margin-top: 30px;">Send code again</a>
-            <a href="#" data-toggle="modal" data-target="#exampleModal" style="width: 100%; text-align: center;">Change phone number</a>
+            <a href="#" data-toggle="modal" data-target="#exampleModal" onclick="editMobileNumer('<?php echo $verificationText; ?>')" style="width: 100%; text-align: center;">Change phone number</a>
 
         </div>
     </div>
@@ -68,72 +67,51 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Update Number</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <div class="col-md-12 pt-3">
-                    <input type="text" class="form-control form-padding red-color" name="mobile_no" id="mobile_no" placeholder="Mobile No" value=<?php echo $vcMobileNo; ?>>
+                    <input type="number" class="form-control form-padding red-color only-decimal" name="mobile_no" onKeyPress="if(this.value.length==10) return false;" id="mobile_no" placeholder="Mobile No">
                 </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" id="btnUpdateNumber" class="btn btn-primary">Update Number</button>
             </div>
         </div>
     </div>
 </div>
 
-<!-- Button trigger modal
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
-
-Modal
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div> -->
-
 <script>
     $(document).ready(function() {
 
-        $('#exampleModal').on('shown.bs.modal', function() {
-            $('#myInput').trigger('focus')
-        })
+        $('.only-decimal').keypress(function(event) {
+            return isNumber(event, this)
+        });
+
 
         $("#otpResend").click(function() {
             // alert("Handler for .click() called.");
             var EmailVerificationCode = $("#EmailVerificationCode").val();
             // alert(EmailVerificationCode);
             if (EmailVerificationCode) {
-                console.log('in ajax');
                 $.ajax({
                     url: base_url + 'registration/otpResend/' + EmailVerificationCode,
                     type: 'post',
                     dataType: 'json',
                     async: true,
                     success: function(response) {
-
                         // alert(response.messages);
-                        swal("Good job!", response.messages, "success");
+
+                        if (response.success === true) {
+                            swal("Sent Successfully!", response.messages, "success");
+                        } else {
+                            swal("Can't Send!", response.messages, "error");
+                        }
+
                     },
                     error: function(xhr, status, error) {
                         //var err = eval("(" + xhr.responseText + ")");
@@ -141,51 +119,110 @@ Modal
                     }
                 });
             }
+        });
+
+
+        $("#btnUpdateNumber").click(function() {
+            var mobile_no = $("#mobile_no").val();
+            var emailVerificationCode = $("#EmailVerificationCode").val();
+            const length = $("#mobile_no").val().length;
+            
+            if (length != 10) {
+                toastr["error"]("Please Valid Mobile No");
+                $("#mobile_no").focus();
+            } else {
+                if (mobile_no) {
+                    $.ajax({
+                        url: base_url + 'registration/upDateMobileNumber/' + mobile_no + '/' + emailVerificationCode,
+                        type: 'post',
+                        dataType: 'json',
+                        async: true,
+                        success: function(response) {
+                            if (response.success === true) {
+                                toastr['success']('Update Successfully');
+                                $("#exampleModal").modal('hide');
+                            } else {
+                                swal("Error!", response.messages, "error");
+                            }
+                        },
+                        error: function(xhr, status, error) {
+                            //var err = eval("(" + xhr.responseText + ")");
+                            alert(xhr.responseText);
+                        }
+                    });
+                }
+            }
 
         });
 
-        // document.getElementById("otpResend").addEventListener("click", myFunction);
+        $("#btnVerify").click(function() {
+            var emailVerificationCode = $("#EmailVerificationCode").val();
+            var no1 = $("#no1").val();
+            var no2 = $("#no2").val();
+            var no3 = $("#no3").val();
+            var no4 = $("#no4").val();
 
-        // function myFunction() {
-        //     var EmailVerificationCode = $("#EmailVerificationCode").val();
-        //     alert(EmailVerificationCode);
-        //     if (EmailVerificationCode > 0) {
-        //         $.ajax({
-        //             url: base_url + 'registration/otpResend/' + EmailVerificationCode,
-        //             type: 'post',
-        //             dataType: 'json',   
-        //             async: true,
-        //             success: function(response) {
+            var otpNumber = no1 + no2 + no3 + no4
 
-        //                 alert(response.messages);
-        //             },
-        //             error: function(xhr, status, error) {
-        //                 //var err = eval("(" + xhr.responseText + ")");
-        //                 alert(xhr.responseText);
-        //             }
-        //         });
-        //     }
-        // }
+            if (jQuery.trim($("#no1").val()).length == 0) {
+                toastr["error"]("Please Enter No 1");
+                $("#no1").focus();
+            } else if (jQuery.trim($("#no2").val()).length == 0) {
+                toastr["error"]("Please Enter No 2");
+                $("#no2").focus();
+            } else if (jQuery.trim($("#no3").val()).length == 0) {
+                toastr["error"]("Please Enter No 3");
+                $("#no3").focus();
+            } else if (jQuery.trim($("#no4").val()).length == 0) {
+                toastr["error"]("Please Enter No 4");
+                $("#no4").focus();
+            } else {
 
+                if (mobile_no) {
+                    $.ajax({
+                        url: base_url + 'registration/otpVerification/' + otpNumber + '/' + emailVerificationCode,
+                        type: 'post',
+                        dataType: 'json',
+                        async: true,
+                        success: function(response) {
+                            if (response.success === true) {
+                                swal("Successfully!", response.messages, "success");
+                                window.location.href = "<?= base_url('Registration/physicalStatus') ?>";
+                            } else {
+                                swal("Error!", response.messages, "error");
+                            }
+                        },
+                        error: function(xhr, status, error) {
+                            //var err = eval("(" + xhr.responseText + ")");
+                            alert(xhr.responseText);
+                        }
+                    });
+                }
+            }
+
+        });
 
     });
 
-    // function doSomething() {
-    //     var EmailVerificationCode = $("#EmailVerificationCode").val();
-    //     if (EmailVerificationCode > 0) {
-    //         $.ajax({
-    //             url: base_url + 'request/'otpResend/' + EmailVerificationCode,
-    //             type: 'post',
-    //             dataType: 'json',
-    //             success: function(response) {
+    function isNumber(evt, element) {
+        var charCode = (evt.which) ? evt.which : event.keyCode
+        if (
+            (charCode != 46 || $(element).val().indexOf('.') != -1) && // “.” CHECK DOT, AND ONLY ONE.
+            (charCode < 48 || charCode > 57) &&
+            (charCode != 13)) {
+            return false;
+        }
+        return true;
+    }
 
-    //                 alert("yaaa");
-    //             },
-    //             error: function(xhr, status, error) {
-    //                 //var err = eval("(" + xhr.responseText + ")");
-    //                 alert(xhr.responseText);
-    //             }
-    //         });
-    //     }
-    // }
+    function editMobileNumer(EmailVerificationCode) {
+        $.ajax({
+            url: base_url + 'registration/fetchUserDate/' + EmailVerificationCode,
+            type: 'post',
+            dataType: 'json',
+            success: function(response) {
+                $("#mobile_no").val(response.Without94);
+            }
+        });
+    }
 </script>
