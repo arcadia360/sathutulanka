b576ead2f734cd56a72c5bf959f722cf27cdcd81
Ramodk <42975445+Ramodk@users.noreply.github.com>
2021-05-03 19:40:53 +0530

    Update Registration.php

--- application/controllers/Registration.php
+++ application/controllers/Registration.php
@@ -195,14 +195,31 @@ class Registration extends Admin_Controller
 		$random_EmailCode = "";
 		$random_EmailCode = substr(md5(uniqid(rand(), true)), 16, 16);
 
-		$response = $this->Model_registration->saveCreateAccount($email, $random_EmailCode);
-		$this->Model_registration->sendVerificatinEmail($email, $random_EmailCode);
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[user.vcEmail]');
 
-		if ($response['success'] == true) {
+		$this->form_validation->set_error_delimiters('<p class="text-danger">', '</p>');
 
-			$this->load->view('registration/create_account');
-		} else {
+		
+		// $this->Model_registration->sendVerificatinEmail($email, $random_EmailCode);
+		if ($this->form_validation->run() == TRUE) {
+			$response = $this->Model_registration->saveCreateAccount($email, $random_EmailCode);
+			if ($response['success'] == true) {
+				$this->Model_registration->sendVerificatinEmail($email, $random_EmailCode);
+				$response['messages'] = "Please Check Your Email";
+				$response['success'] = true;
+			} else {
+				$response['messages'] = "Sent error";
+				$response['success'] = false;
+			}
+		}else{
+			$response['success'] = false;
+			foreach ($_POST as $key => $value) {
+				$response['messages'][$key] = form_error($key);
+			}
 		}
+	
+
+		echo json_encode($response);
 	}
 
 	public function EmailVerification($verificationText = NULL)
@@ -212,7 +229,7 @@ class Registration extends Admin_Controller
 		$userData = $this->Model_registration->getUserDate($verificationText);
 		$noRecords = $this->Model_registration->verifyEmailAddress($verificationText);
 		$noRecordsendOTP = 0;
-	
+
 		if ($noRecords > 0) {
 			$data['verificationText'] = $verificationText;
 			$data['vcMobileNo'] = $userData['Without94'];
@@ -231,6 +248,15 @@ class Registration extends Admin_Controller
 			}
 		} else {
 			$error = array('error' => "Sorry Unable to Verify Your Email!");
+			$this->load->helper('language');
+			$session_data = $this->session->userdata();
+			if ($session_data['language_id'] == 1) { // English
+				$this->lang->load('en', 'English');
+			} else if ($session_data['language_id'] == 2) { // Sinhala
+				$this->lang->load('si', 'Sinhala');
+			} else if ($session_data['language_id'] == 3) { // Tamil
+				$this->lang->load('ta', 'Tamil');
+			}
 			$this->load->view('registration/create_account');
 		}
 		// $data['errormsg'] = $error;
@@ -240,20 +266,19 @@ class Registration extends Admin_Controller
 	{
 		// var_dump($verificationText);
 		// $this->render_template_registration('Registration/otp_verification', 'OTP Verification', NULL);
-		
+
 		$userData = $this->Model_registration->getUserDate($verificationText);
 
 		if ($userData['intOTPSentCount'] == 3) {
 			$response['messages'] = "Can't Send More than 3 text messages, Please Contact Sathutu Lanka";
 			$response['success'] = false;
-		}
-		else{
+		} else {
 			$noRecordsendOTP = $this->Model_registration->sendOTP($verificationText, true);
 			if ($noRecordsendOTP > 0) {
 				$response['messages'] = "Sent Successfully";
 				$response['success'] = true;
 			} else {
-	
+
 				$response['messages'] = "Sent error";
 				$response['success'] = false;
 			}
@@ -271,9 +296,9 @@ class Registration extends Admin_Controller
 		return false;
 	}
 
-	public function otpVerification($otpNumber,$emailVerificationCode)
+	public function otpVerification($otpNumber, $emailVerificationCode)
 	{
-		$noRecord = $this->Model_registration->otpVerification($otpNumber,$emailVerificationCode);
+		$noRecord = $this->Model_registration->otpVerification($otpNumber, $emailVerificationCode);
 		if ($noRecord > 0) {
 			$response['messages'] = "Verification Successfully";
 			$response['success'] = true;
@@ -284,15 +309,15 @@ class Registration extends Admin_Controller
 		echo json_encode($response);
 	}
 
-	public function upDateMobileNumber($mobile_no,$emailVerificationCode)
+	public function upDateMobileNumber($mobile_no, $emailVerificationCode)
 	{
-		$noRecord = $this->Model_registration->upDateMobileNumber($mobile_no,$emailVerificationCode);
+		$noRecord = $this->Model_registration->upDateMobileNumber($mobile_no, $emailVerificationCode);
 		if ($noRecord > 0) {
 			$response['messages'] = "Update Successfully";
 			$response['success'] = true;
 		} else {
 
-			$response['messages'] = "Update error";
+			$response['messages'] = "Nothing To Change";
 			$response['success'] = false;
 		}
 		echo json_encode($response);
