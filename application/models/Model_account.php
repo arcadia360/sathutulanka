c2bf06a53e99385834573f1d8b218daae1c64552
Ramodk <42975445+Ramodk@users.noreply.github.com>
2021-05-03 19:40:12 +0530

    Create Model_account.php

---
+++ application/models/Model_account.php
@@ -0,0 +1,18 @@
+<?php
+class Model_account extends CI_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getUserDate($UserID)
+    {
+      $sql = "SELECT intUserID, vcNickName, vcCountryCode, vcMobileNo, vcPassword, vcEmail, vcProvidingInformationType, vcGender, dtDOB, vcMaritalStatus, vcNoOfChildren, intUserAccountStatusTypeID, vcMarriageType, intNoOfSubmitedForm, intHeight, intWeightFrom, intWeightTo, intBodyTypeID, intSkinColourID, isDisability, vcBloodGroup, isHealthInfo, vcMotherTounge, vcEthnicity, vcReligion, isPoliceReportCanProvide
+      FROM user WHERE intUserID =   ? ";
+      $query = $this->db->query($sql, array($UserID));
+      return $query->row_array();
+    }
+  
+}
