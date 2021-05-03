323f355ab7704d2da3adc94dca2541fd90b4f524
Ramodk <42975445+Ramodk@users.noreply.github.com>
2021-05-03 19:40:58 +0530

    Update Account.php

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -4,14 +4,23 @@ class Account extends Admin_Controller{
     public function __construct()
 	{
 		parent::__construct();
-		// $this->load->model('Model_registration');
+		$this->load->model('Model_account');
 		// header("Cache-Control: no-cache, must-revalidate"); // HTTP 1.1.
 		// header("Pragma: no-cache"); // HTTP 1.0.
 		// header("Expires: 0"); // Proxies.
 	}
 
     public function MyAccount(){
-		$this->render_template('account/my_account', 'My Account', null);
+
+		$user_id = $this->session->userdata('user_id');
+		$user_id= 18;
+		$userData = $this->Model_account->getUserDate($user_id);
+
+		$this->data['userData'] = $userData;
+
+		$this->render_template('account/my_account', 'My Account',  $this->data);
+
+		
 	}
 
 }
