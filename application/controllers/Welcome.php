e6aa54a043ed4214df8c5c626b1d3cf97ce2b74b
Viraj <virajlasithazeero@gmail.com>
2021-05-15 01:27:06 +0530

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -7,7 +7,7 @@ class Welcome extends Admin_Controller {
 	{
 		// $this->CheckAndRedirectNextForm();
 
-		if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {  // If Fresh User
+		if (!isset($_SESSION['member_id']) || empty($_SESSION['member_id'])) {  // If Fresh User
 			$this->render_template('welcome', 'W E L C O M E', null);
 		} else{
 			$this->CheckAndRedirectNextForm();
@@ -17,7 +17,7 @@ class Welcome extends Admin_Controller {
 	}
 
 	public function ChangeLanguage($LanguageID,$url){
-		if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {  // If Fresh User
+		if (!isset($_SESSION['member_id']) || empty($_SESSION['member_id'])) {  // If Fresh User
 			$logged_in_sess = array(
 				'language_id' => $LanguageID // 1 - English, 2 - Sinhala, 3 - Tamil
 			);
