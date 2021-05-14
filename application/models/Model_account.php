ccc7cfa1a243b5ca04873fb081bb73eb90fb1753
dhanu159 <randimakumanayake@yahoo.com>
2021-05-14 21:43:22 +0530

    Update Model_account.php

--- application/models/Model_account.php
+++ application/models/Model_account.php
@@ -2,17 +2,16 @@
 class Model_account extends CI_Model
 {
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
+  public function __construct()
+  {
+    parent::__construct();
+  }
 
-    public function getUserDate($UserID)
-    {
-      $sql = "SELECT intUserID, vcNickName, vcCountryCode, vcMobileNo, vcPassword, vcEmail, vcProvidingInformationType, vcGender, dtDOB, vcMaritalStatus, vcNoOfChildren, intUserAccountStatusTypeID, vcMarriageType, intNoOfSubmitedForm, intHeight, intWeightFrom, intWeightTo, intBodyTypeID, intSkinColourID, isDisability, vcBloodGroup, isHealthInfo, vcMotherTounge, vcEthnicity, vcReligion, isPoliceReportCanProvide
-      FROM user WHERE intUserID =   ? ";
-      $query = $this->db->query($sql, array($UserID));
-      return $query->row_array();
-    }
-  
+  public function getUserDate($UserID)
+  {
+    $sql = "SELECT intUserID, vcNickName, vcCountryCode, vcMobileNo, vcPassword, vcEmail, vcProvidingInformationType, vcGender, dtDOB, vcMaritalStatus, vcNoOfChildren, intUserAccountStatusTypeID, vcMarriageType, intNoOfSubmitedForm, intHeight, intWeightFrom, intWeightTo, intBodyTypeID, intSkinColourID, isDisability, vcBloodGroup, isHealthInfo, vcMotherTounge, vcEthnicity, vcReligion, isPoliceReportCanProvide
+      FROM member WHERE intUserID =   ? ";
+    $query = $this->db->query($sql, array($UserID));
+    return $query->row_array();
+  }
 }
