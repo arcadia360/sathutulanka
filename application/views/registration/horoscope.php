1f559e8ceecee63cfaf3d2f06d0b175c005fb35e
dhanu159 <randimakumanayake@yahoo.com>
2021-05-25 08:49:38 +0530

    Update horoscope.php

--- application/views/registration/horoscope.php
+++ application/views/registration/horoscope.php
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="offset-lg-2 col-lg-8 col-md-12 col-sm-12 border rounded main-section">
         <h3 class="text-center text-inverse title">Horoscope</h3>
-        <!-- <hr> -->
+        <hr>
         <div class="row">
           <div class="container">
             <button class="btn btn-info" style="width: 150px; margin-bottom:20px; float:right;" type="button" id="btnSkip">Skip this step &nbsp; <i class="fas fa-forward"></i></button>
@@ -18,21 +18,21 @@
               <div class="row">
                 <div class="col-4">
                   <label class="custom-control custom-radio">
-                    <input id="Traditional" name="Family-Values" type="radio" class="custom-control-input" value="Very Important">
+                    <input id="VeryImportan" name="matchingHoroscope" type="radio" class="custom-control-input" value="Very Important" checked>
                     <span class="custom-control-indicator"></span>
                     <span class="custom-control-description">Very Important</span>
                   </label>
                 </div>
                 <div class="col-4">
                   <label class="custom-control custom-radio">
-                    <input id="TraditionalAndModern" name="Family-Values" type="radio" class="custom-control-input" value="Important">
+                    <input id="Important" name="matchingHoroscope" type="radio" class="custom-control-input" value="Important">
                     <span class="custom-control-indicator"></span>
                     <span class="custom-control-description">Important</span>
                   </label>
                 </div>
                 <div class="col-4">
                   <label class="custom-control custom-radio">
-                    <input id="Modern" name="Family-Values" type="radio" class="custom-control-input" value="Not Important">
+                    <input id="Not Important" name="matchingHoroscope" type="radio" class="custom-control-input" value="Not Important">
                     <span class="custom-control-indicator"></span>
                     <span class="custom-control-description">Not Important</span>
                   </label>
@@ -47,7 +47,7 @@
             </div>
             <div class="col-lg-8 col-12">
               <div class="form-group">
-                <select class="custom-select d-block form-control" id="bloodGroup" name="bloodGroup" required>
+                <select class="custom-select d-block form-control" id="ZodiacSign" name="ZodiacSign" required>
                   <option value="0"><?= lang('select') ?></option>
                   <option value="Aries">Aries</option>
                   <option value="Taurus">Taurus</option>
@@ -72,7 +72,7 @@
             </div>
             <div class="col-lg-8 col-12">
               <div class="form-group">
-                <select class="custom-select d-block form-control" id="bloodGroup" name="bloodGroup" required>
+                <select class="custom-select d-block form-control" id="Ganaya" name="Ganaya" required>
                   <option value="0"><?= lang('select') ?></option>
                   <option value="Dewa">Dewa</option>
                   <option value="Manushya">Manushya</option>
@@ -88,7 +88,7 @@
             </div>
             <div class="col-lg-8 col-12">
               <div class="form-group">
-                <select class="custom-select d-block form-control" id="bloodGroup" name="bloodGroup" required>
+                <select class="custom-select d-block form-control" id="Nekatha" name="Nekatha" required>
                   <option value="0"><?= lang('select') ?></option>
                   <option value="Asvida">Asvida</option>
                   <option value="Berana">Berana</option>
@@ -359,21 +359,21 @@
               <div class="row">
                 <div class="col-4">
                   <label class="custom-control custom-radio">
-                    <input id="Traditional" name="Family-Values" type="radio" class="custom-control-input" value="Dont Know">
+                    <input name="PapaKendara" type="radio" class="custom-control-input" value="Dont Know" checked>
                     <span class="custom-control-indicator"></span>
                     <span class="custom-control-description">Dont Know</span>
                   </label>
                 </div>
                 <div class="col-4">
                   <label class="custom-control custom-radio">
-                    <input id="TraditionalAndModern" name="Family-Values" type="radio" class="custom-control-input" value="Yes">
+                    <input name="PapaKendara" type="radio" class="custom-control-input" value="Yes">
                     <span class="custom-control-indicator"></span>
                     <span class="custom-control-description">Yes</span>
                   </label>
                 </div>
                 <div class="col-4">
                   <label class="custom-control custom-radio">
-                    <input id="Modern" name="Family-Values" type="radio" class="custom-control-input" value="No">
+                    <input name="PapaKendara" type="radio" class="custom-control-input" value="No">
                     <span class="custom-control-indicator"></span>
                     <span class="custom-control-description">No</span>
                   </label>
@@ -381,7 +381,7 @@
               </div>
             </div>
           </div>
-
+          <hr>
           <div class="row">
             <div class="col-lg-6 col-sm-6 col-6 text-center" style="padding: 10px;">
               <button class="btn btn-info" style="width: 150px;" id="btnBack" type="button">BACK</button>
@@ -390,8 +390,6 @@
               <button class="btn btn-info" style="width: 150px;" type="button" id="btnSubmit">CONTINUE</button>
             </div>
           </div>
-
-
         </form>
 
       </div>
@@ -405,5 +403,98 @@
     $('#btnBack').click(function() {
       window.location.href = "<?php echo base_url('Registration/afterMarriage') ?>";
     });
+
+    $('#btnSubmit').click(function() {
+      var isMatchingHoroscopeSelected = $("input[name=matchingHoroscope]").is(":checked");
+      var isPapaKendaraSelected = $("input[name=PapaKendara]").is(":checked");
+      if (!isMatchingHoroscopeSelected) {
+        toastr["error"]("Please select matching horoscope");
+      } else if ($('#ZodiacSign').val() == 0) {
+        toastr["error"]("Please select zodiac sign");
+        $("#ZodiacSign").focus();
+      } else if ($('#Ganaya').val() == 0) {
+        toastr["error"]("Please select ganaya");
+        $("#Ganaya").focus();
+      } else if ($('#Nekatha').val() == 0) {
+        toastr["error"]("Please select nekatha");
+        $("#Nekatha").focus();
+      } else if ($('#Ravi').val() == 0) {
+        toastr["error"]("Please select ravi");
+        $("#Ravi").focus();
+      } else if ($('#Moon').val() == 0) {
+        toastr["error"]("Please select moon");
+        $("#Moon").focus();
+      } else if ($('#Mars').val() == 0) {
+        toastr["error"]("Please select mars");
+        $("#Mars").focus();
+      } else if ($('#Mercury').val() == 0) {
+        toastr["error"]("Please select mercury");
+        $("#Mercury").focus();
+      } else if ($('#Jupiter').val() == 0) {
+        toastr["error"]("Please select jupiter");
+        $("#Jupiter").focus();
+      } else if ($('#Venus').val() == 0) {
+        toastr["error"]("Please select venus");
+        $("#Venus").focus();
+      } else if ($('#Saturn').val() == 0) {
+        toastr["error"]("Please select saturn");
+        $("#Saturn").focus();
+      } else if ($('#Kethu').val() == 0) {
+        toastr["error"]("Please select kethu");
+        $("#Kethu").focus();
+      } else if (!isPapaKendaraSelected) {
+        toastr["error"]("Please select papa kendara");
+      } else {
+        var form = $("#addHoroscopeDetails");
+        $.ajax({
+          type: form.attr('method'),
+          url: form.attr('action'),
+          data: form.serialize(),
+          dataType: 'json',
+          success: function(response) {
+            if (response.success == true) {
+              Swal.fire({
+                icon: 'success',
+                title: 'Horoscope details saved successfully!',
+                showConfirmButton: false,
+                timer: 2000
+              }).then((result) => {
+                if (result.dismiss === Swal.DismissReason.timer) {
+                  window.location.href = "<?= base_url('Registration/myPhotosAndVideos') ?>";
+                }
+              })
+
+            } else {
+              if (response.messages instanceof Object) {
+                $.each(response.messages, function(index, value) {
+                  var id = $("#" + index);
+                  id.closest('.form-group')
+                    .removeClass('has-error')
+                    .removeClass('has-success')
+                    .addClass(value.length > 0 ? 'has-error' : 'has-success');
+                  id.after(value);
+                });
+              } else {
+                toastr["error"](response.messages);
+                $(button).prop('disabled', false);
+              }
+            }
+          },
+          error: function() {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Internal Server Error! Please contact system administrator.',
+            })
+          }
+        });
+      }
+    });
+
+    $('#btnSkip').click(function() {
+      window.location.href = "<?php echo base_url('Registration/myPhotosAndVideos') ?>";
+    });
+
+
   });
 </script>
