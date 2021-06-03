04aa5961b6b456e94a8142753844d8979351f549
Viraj <virajlasithazeero@gmail.com>
2021-06-03 13:23:23 +0530

    Update footer.php

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -7,7 +7,9 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
 
 
-
+<!-- UiKit JS -->
+<script src='https://getuikit.com/assets/uikit/dist/js/uikit.js?nc=868'></script>
+<script src='https://getuikit.com/assets/uikit/dist/js/uikit-icons.js?nc=868'></script>
 <!-- Popper JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <!-- Bootstrap 4.0 JS -->
