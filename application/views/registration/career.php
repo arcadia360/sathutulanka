cce48165c3058fe72802a4f34f67779523b6b3ec
Viraj <virajlasithazeero@gmail.com>
2021-06-05 03:56:30 +0530

    Update career.php

--- application/views/registration/career.php
+++ application/views/registration/career.php
@@ -1,152 +1,93 @@
-<div class="btnbg" style="padding-top: 10px;">
-  <!-- Container Area Start -->
-  <div class="container" style="z-index: 1;">
+<div class="offset-lg-3 col-lg-6 main-section">
+  <h3 class="text-center text-inverse title">Career</h3>
+  <form method="post" action="<?= base_url('Registration/addcareerDetails') ?>" id="addcareerDetails">
     <div class="row">
-      <div class="offset-lg-2 col-lg-8 col-md-12 col-sm-12 border rounded main-section">
-        <!-- <h3 class="text-center text-inverse font-weight-bold"><?= lang('residance') ?></h3> -->
-        <h3 class="text-center text-inverse title">Career</h3>
-        <hr>
-        <form method="post" action="<?= base_url('Registration/addcareerDetails') ?>" id="addcareerDetails">
-
-
-          <div class="row">
-            <div class="col-6">
-              <div class="form-group">
-                <!-- <label class="text-inverse font-weight-bold" for="validationCustom01"><?= lang('liveInSriLanka') ?></label> -->
-                <label class="text-inverse font-weight-bold" for="validationCustom01">Working With</label>
-              </div>
-            </div>
-            <div class="col-6">
-              <div class="form-group">
-                <select class="custom-select d-block form-control" id="workingWith" name="workingWith">
-
-                </select>
-              </div>
-            </div>
-          </div>
-          <div id="not-working-or-education">
-            <!-- if not working or education div start -->
-            <div id="working-as">
-              <!-- working-as div start -->
-              <div class="row">
-                <div class="col-6">
-                  <div class="form-group">
-                    <!-- <label class="text-inverse font-weight-bold" for="validationCustom01"><?= lang('liveInSriLanka') ?></label> -->
-                    <label class="text-inverse font-weight-bold" for="validationCustom01">Working As</label>
-                  </div>
-                </div>
-                <div class="col-6">
-                  <div class="form-group">
-                    <select class="custom-select d-block form-control" id="WorkingAsMainCat" name="WorkingAsMainCat">
-
-                    </select>
-                  </div>
-                </div>
-              </div>
-
-              <div class="row">
-                <div class="col-6">
-                </div>
-                <div class="col-6">
-                  <div class="form-group">
-                    <select class="custom-select d-block form-control" id="workingAsSubCat" name="workingAsSubCat">
-
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!-- working-as div end -->
-
-            <div class="row">
-              <div class="col-6">
-                <div class="form-group">
-                  <!-- <label class="text-inverse font-weight-bold" for="validationCustom01"><?= lang('liveInSriLanka') ?></label> -->
-                  <label class="text-inverse font-weight-bold" for="validationCustom01">Working Location</label>
-                </div>
-              </div>
-              <div class="col-6">
-                <div class="form-group">
-                  <select class="custom-select d-block form-control" id="workingLocation" name="workingLocation">
-                    <option value="0"><?= lang('select') ?></option>
-                    <option value="Out Side Sri Lanka">Out Side Sri Lanka</option>
-                    <option value="In Sri Lanka">In Sri Lanka</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-
-            <div class="row" id="ifWorkingLocationSL">
-              <div class="col-6">
-
-              </div>
-              <div class="col-3">
-                <div class="form-group">
-                  <select class="custom-select d-block form-control" id="district" name="district">
-
-                  </select>
-                </div>
-              </div>
-              <div class="col-3">
-                <div class="form-group">
-                  <select class="custom-select d-block form-control" id="city" name="city">
-
-                  </select>
-                </div>
-              </div>
-            </div>
-
-            <div class="row" id="ifWorkingLocationOS">
-              <div class="col-6">
-
-              </div>
-              <div class="col-6">
-                <div class="row">
-                  <div class="col-12">
-                    <div class="form-group">
-                      <select class="custom-select d-block form-control" id="country" name="country">
-                      </select>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
-
-            <div class="row">
-              <div class="col-6">
-                <div class="form-group">
-                  <!-- <label class="text-inverse font-weight-bold" for="validationCustom01"><?= lang('liveInSriLanka') ?></label> -->
-                  <label class="text-inverse font-weight-bold" for="validationCustom01">Describe Career</label>
-                </div>
-              </div>
-              <div class="col-6">
-                <div class="form-group">
-                  <textarea class="form-control" id="describeCareer" rows="5" name="describeCareer"></textarea>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- not working or education div end -->
-
-
-          <hr>
-          <div class="row">
-            <div class="col-lg-6 col-sm-6 col-6 text-center" style="padding: 10px;">
-              <button class="btn btn-info" style="width: 150px;" id="btnBack" type="button">Back</button>
-            </div>
-            <div class="col-lg-6 col-sm-6 col-6 text-center" style="padding: 10px;">
-              <button class="btn btn-info" style="width: 150px;" type="button" id="btnSubmit">CONTINUE</button>
-            </div>
-          </div>
-        </form>
+      <div class="col-12">
+        <label class="text-inverse font-weight-bold" for="validationCustom01">Working With</label>
+        <div class="form-group">
+          <select class="custom-select d-block form-control" id="workingWith" name="workingWith">
+            <!-- Generated Area -->
+          </select>
+        </div>
       </div>
     </div>
-  </div>
-  <!-- Container Area End -->
-
+    <div id="not-working-or-education">
+      <div id="working-as">
+        <div class="row">
+          <div class="col-12">
+            <label class="text-inverse font-weight-bold" for="validationCustom01">Working As</label>
+            <div class="form-group">
+              <select class="custom-select d-block form-control" id="WorkingAsMainCat" name="WorkingAsMainCat">
+                <!-- Generated Area -->
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <div class="form-group">
+              <select class="custom-select d-block form-control" id="workingAsSubCat" name="workingAsSubCat">
+                <!-- Generated Area -->
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <label class="text-inverse font-weight-bold" for="validationCustom01">Working Location</label>
+          <div class="form-group">
+            <select class="custom-select d-block form-control" id="workingLocation" name="workingLocation">
+              <option value="0"><?= lang('select') ?></option>
+              <option value="Out Side Sri Lanka">Out Side Sri Lanka</option>
+              <option value="In Sri Lanka">In Sri Lanka</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="row" id="ifWorkingLocationSL">
+        <div class="col-12">
+          <div class="form-group">
+            <select class="custom-select d-block form-control" id="district" name="district">
+              <!-- Generated Area -->
+            </select>
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="form-group">
+            <select class="custom-select d-block form-control" id="city" name="city">
+              <!-- Generated Area -->
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="row" id="ifWorkingLocationOS">
+        <div class="col-12">
+          <div class="form-group">
+            <select class="custom-select d-block form-control" id="country" name="country">
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <label class="text-inverse font-weight-bold" for="validationCustom01">Describe Career</label>
+          <div class="form-group">
+            <textarea class="form-control" id="describeCareer" rows="5" name="describeCareer"></textarea>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-12">
+        <button class="btn btn-info" id="btnBack" type="button"><i class="fas fa-angle-double-left"></i> &nbsp; BACK</button>
+        <button class="btn btn-info float-right" type="button" id="btnSubmit">Next &nbsp; <i class="fas fa-angle-double-right"></i></button>
+      </div>
+    </div>
+  </form>
 </div>
+
 
 <script>
   $(function() {
@@ -195,35 +136,35 @@
 
     $('#btnSubmit').click(function() {
       if ($('#workingWith').val() == 0) {
-        toastr["error"]("Please select working with");
+        toastr["error"]("Please select working with !");
         $("#workingWith").focus();
 
       } else if ($('#workingWith').val() != 6 && $('#workingWith').val() != 7 && $('#workingWith').val() != 8 && ($('#workingWith').val() != 9) && $('#WorkingAsMainCat').val() == 0) {
-        toastr["error"]("Please select working as main category");
+        toastr["error"]("Please select working as main category !");
         $("#WorkingAsMainCat").focus();
 
       } else if ($("#WorkingAsMainCat").val() != 0 && $('#workingAsSubCat').val() == 0) {
-        toastr["error"]("Please select working as sub category");
+        toastr["error"]("Please select working as sub category !");
         $("#workingAsSubCat").focus();
 
       } else if ($('#workingWith').val() != 8 && $('#workingWith').val() != 9 && $('#workingLocation').val() == 0) {
-        toastr["error"]("Please select working location");
+        toastr["error"]("Please select working location !");
         $("#workingLocation").focus();
 
       } else if ($('#workingLocation').val() == "In Sri Lanka" && $('#district').val() == 0) {
-        toastr["error"]("Please select working district");
+        toastr["error"]("Please select working district !");
         $("#district").focus();
 
       } else if ($('#workingLocation').val() == "In Sri Lanka" && $('#city').val() == 0) {
-        toastr["error"]("Please select working city");
+        toastr["error"]("Please select working city !");
         $("#city").focus();
 
       } else if ($('#workingLocation').val() == "Out Side Sri Lanka" && $('#country').val() == 0) {
-        toastr["error"]("Please select country");
+        toastr["error"]("Please select country !");
         $("#country").focus();
 
       } else if ($("#workingLocation").val() != 0 && $('#describeCareer').val() == 0) {
-        toastr["error"]("Please describe career");
+        toastr["error"]("Please describe career !");
         $("#describeCareer").focus();
       } else {
         var form = $("#addcareerDetails");
