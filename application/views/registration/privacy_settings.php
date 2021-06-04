b16a4f818609da5fcaed5e2cfa29831d49d8a228
Viraj <virajlasithazeero@gmail.com>
2021-06-05 03:56:55 +0530

    Update privacy_settings.php

--- application/views/registration/privacy_settings.php
+++ application/views/registration/privacy_settings.php
@@ -1,94 +1,86 @@
-<div class="btnbg" style="padding-top: 10px;">
-  <!-- Container Area Start -->
-  <div class="container" style="z-index: 1;">
+<div class="offset-lg-3 col-lg-6 main-section">
+  <h3 class="text-center text-inverse title">Privacy Settings</h3>
+  <form method="post" action="<?= base_url('Registration/AddPrivacySettings') ?>" id="AddPrivacySettings">
+    <div class="text-center">
+      <h5>Below field is mandatory.</h5>
+    </div>
+    <br>
     <div class="row">
-      <div class="offset-lg-2 col-lg-8 col-md-12 col-sm-12 border rounded main-section">
-        <h3 class="text-center text-inverse title">Privacy Settings</h3>
-        <hr>
-        <form method="post" action="<?= base_url('Registration/AddPrivacySettings') ?>" id="AddPrivacySettings">
-          <div class="text-center">
-            <h5>Below field is mandatory.</h5>
-          </div>
-          <div class="row">
-            <div class="col-3"></div>
-            <div class="col-3 text-al-center"><label for="">Visible to all members</label></div>
-            <div class="col-3 text-al-center"><label for="">visible to verified members</label></div>
-            <div class="col-3 text-al-center"><label for="">open on request</label></div>
-          </div>
-          <div class="row">
-            <div class="col-3"><label for="">(1) My Photos</label></div>
-            <div class="col-3 text-al-center">
-              <input name="MyPhotos" type="radio" value="1">
-            </div>
-            <div class="col-3 text-al-center">
-              <input name="MyPhotos" type="radio" value="2">
-            </div>
-            <div class="col-3 text-al-center">
-              <input name="MyPhotos" type="radio" value="3">
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-3"><label for="">(2) My Videos</label></div>
-            <div class="col-3 text-al-center">
-              <input name="MyVideos" type="radio" value="1">
-            </div>
-            <div class="col-3 text-al-center">
-              <input name="MyVideos" type="radio" value="2">
-            </div>
-            <div class="col-3 text-al-center">
-              <input name="MyVideos" type="radio" value="3">
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-3"><label for="">(3) Assets Details</label></div>
-            <div class="col-3 text-al-center">
-              <input name="AssetsDetails" type="radio" value="1">
-            </div>
-            <div class="col-3 text-al-center">
-              <input name="AssetsDetails" type="radio" value="2">
-            </div>
-            <div class="col-3 text-al-center">
-              <input name="AssetsDetails" type="radio" value="3">
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-3"><label for="">(4) Family Details</label></div>
-            <div class="col-3 text-al-center">
-              <input name="FamilyDetails" type="radio" value="1">
-            </div>
-            <div class="col-3 text-al-center">
-              <input name="FamilyDetails" type="radio" value="2">
-            </div>
-            <div class="col-3 text-al-center">
-              <input name="FamilyDetails" type="radio" value="3">
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-3"><label for="">(5) Horoshcope</label></div>
-            <div class="col-3 text-al-center">
-              <input name="Horoshcope" type="radio" value="1">
-            </div>
-            <div class="col-3 text-al-center">
-              <input name="Horoshcope" type="radio" value="2">
-            </div>
-            <div class="col-3 text-al-center">
-              <input name="Horoshcope" type="radio" value="3">
-            </div>
-          </div>
-          <hr>
-          <div class="row">
-            <div class="col-lg-6 col-sm-6 col-6 text-center" style="padding: 10px;">
-              <button class="btn btn-info" style="width: 150px;" id="btnBack" type="button">Back</button>
-            </div>
-            <div class="col-lg-6 col-sm-6 col-6 text-center" style="padding: 10px;">
-              <button class="btn btn-info" style="width: 150px;" type="button" id="btnSubmit">CONTINUE</button>
-            </div>
-          </div>
-        </form>
+      <div class="col-3"></div>
+      <div class="col-3 text-al-center"><label for="">Visible to all members</label></div>
+      <div class="col-3 text-al-center"><label for="">visible to verified members</label></div>
+      <div class="col-3 text-al-center"><label for="">open on request</label></div>
+    </div>
+    <div class="row">
+      <div class="col-3"><label for="">(1) My Photos</label></div>
+      <div class="col-3 text-al-center">
+        <input name="MyPhotos" type="radio" value="1">
+      </div>
+      <div class="col-3 text-al-center">
+        <input name="MyPhotos" type="radio" value="2">
+      </div>
+      <div class="col-3 text-al-center">
+        <input name="MyPhotos" type="radio" value="3">
       </div>
     </div>
-  </div>
+    <div class="row">
+      <div class="col-3"><label for="">(2) My Videos</label></div>
+      <div class="col-3 text-al-center">
+        <input name="MyVideos" type="radio" value="1">
+      </div>
+      <div class="col-3 text-al-center">
+        <input name="MyVideos" type="radio" value="2">
+      </div>
+      <div class="col-3 text-al-center">
+        <input name="MyVideos" type="radio" value="3">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3"><label for="">(3) Assets Details</label></div>
+      <div class="col-3 text-al-center">
+        <input name="AssetsDetails" type="radio" value="1">
+      </div>
+      <div class="col-3 text-al-center">
+        <input name="AssetsDetails" type="radio" value="2">
+      </div>
+      <div class="col-3 text-al-center">
+        <input name="AssetsDetails" type="radio" value="3">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3"><label for="">(4) Family Details</label></div>
+      <div class="col-3 text-al-center">
+        <input name="FamilyDetails" type="radio" value="1">
+      </div>
+      <div class="col-3 text-al-center">
+        <input name="FamilyDetails" type="radio" value="2">
+      </div>
+      <div class="col-3 text-al-center">
+        <input name="FamilyDetails" type="radio" value="3">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3"><label for="">(5) Horoshcope</label></div>
+      <div class="col-3 text-al-center">
+        <input name="Horoshcope" type="radio" value="1">
+      </div>
+      <div class="col-3 text-al-center">
+        <input name="Horoshcope" type="radio" value="2">
+      </div>
+      <div class="col-3 text-al-center">
+        <input name="Horoshcope" type="radio" value="3">
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-12">
+        <button class="btn btn-info" id="btnBack" type="button"><i class="fas fa-angle-double-left"></i> &nbsp; BACK</button>
+        <button class="btn btn-info float-right" type="button" id="btnSubmit">Next &nbsp; <i class="fas fa-angle-double-right"></i></button>
+      </div>
+    </div>
+  </form>
 </div>
+
 
 <script>
   $(function() {
@@ -103,15 +95,15 @@
       let isFamilyDetailsSelected = $("input[name=FamilyDetails]").is(":checked");
       let isHoroshcopeSelected = $("input[name=Horoshcope]").is(":checked");
       if (!isMyPhotosSelected) {
-        toastr["error"]("Please select my photos privacy");
+        toastr["error"]("Please select my photos privacy !");
       } else if (!isMyVideosSelected) {
-        toastr["error"]("Please select my videos privacy");
+        toastr["error"]("Please select my videos privacy !");
       } else if (!isAssetsDetailsSelected) {
-        toastr["error"]("Please select asset details privacy");
+        toastr["error"]("Please select asset details privacy !");
       } else if (!isFamilyDetailsSelected) {
-        toastr["error"]("Please select family details privacy");
+        toastr["error"]("Please select family details privacy !");
       } else if (!isHoroshcopeSelected) {
-        toastr["error"]("Please select horoscope privacy");
+        toastr["error"]("Please select horoscope privacy !");
       } else {
         var form = $("#AddPrivacySettings");
         $.ajax({
@@ -123,7 +115,7 @@
             if (response.success == true) {
               Swal.fire({
                 icon: 'success',
-                title: 'Privacy settings saved successfully!',
+                title: 'Privacy settings saved successfully !',
                 showConfirmButton: false,
                 timer: 2000
               }).then((result) => {
