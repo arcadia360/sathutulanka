5e00bc129bf81e4bd25d36ab99ad792252ef96ec
Ramodk <42975445+Ramodk@users.noreply.github.com>
2021-06-12 11:41:34 +0530

    Update Model_registration.php

--- application/models/Model_registration.php
+++ application/models/Model_registration.php
@@ -75,6 +75,71 @@ class Model_registration extends CI_Model
     }
   }
 
+  public function getEnrichmentHobbies($MemberID)
+  {
+    $sql ="SELECT vcEnrichmentHobby, intMID
+    FROM enrichmenthobbies
+    WHERE intMID = ? ";
+    $query = $this->db->query($sql, array($MemberID));
+    return $query->result_array();
+  }
+
+  public function getSportsActivities($MemberID)
+  {
+    $sql ="SELECT vcSportsPhysicalActivities, intMID
+    FROM sportsphysicalactivities
+    WHERE intMID = ? ";
+    $query = $this->db->query($sql, array($MemberID));
+    return $query->result_array();
+  }
+
+  public function getCreativeHobbies($MemberID)
+  {
+    $sql ="SELECT vcCreativeHobbies, intMID
+    FROM creativehobbies
+    WHERE intMID = ? ";
+    $query = $this->db->query($sql, array($MemberID));
+    return $query->result_array();
+  }
+
+  public function getCollectingHobbies($MemberID)
+  {
+    $sql ="SELECT vcCollectingHobby, intMID
+    FROM collectinghobbies
+    WHERE intMID = ? ";
+    $query = $this->db->query($sql, array($MemberID));
+    return $query->result_array();
+  }
+
+  public function getOutdoorsHobbies($MemberID)
+  {
+    $sql ="SELECT vcOutdoorHobies, intMID
+    FROM outdoorhobies
+    WHERE intMID = ? ";
+    $query = $this->db->query($sql, array($MemberID));
+    return $query->result_array();
+  }
+
+  public function getDomesticHobbies($MemberID)
+  {
+    $sql ="SELECT vcdomesticHobbies, intMID
+    FROM domestichobbies
+    WHERE intMID = ? ";
+    $query = $this->db->query($sql, array($MemberID));
+    return $query->result_array();
+  }
+  
+
+  public function getSocialActivites($MemberID)
+  {
+    $sql ="SELECT vcSocialActivities, intMID
+    FROM socialactivities
+    WHERE intMID = ? ";
+    $query = $this->db->query($sql, array($MemberID));
+    return $query->result_array();
+  }
+  
+
   public function loadDisabilityDetails()
   {
     $this->db->select('*');
