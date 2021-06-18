6e474db721c6720048aa24db2b06fd3373500651
Viraj <virajlasithazeero@gmail.com>
2021-06-18 18:29:53 +0530

    Update Account.php

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -14,7 +14,6 @@ class Account extends Admin_Controller{
     public function MyAccount(){
 
 		$member_id = $this->session->userdata('member_id');
-		// $user_id= 18;
 		$memberData = $this->Model_account->getMemberData($member_id);
 
 		$this->data['memberData'] = $memberData;
@@ -22,21 +21,48 @@ class Account extends Admin_Controller{
 		$this->render_template('account/my_account', 'My Account',  $this->data);
 	}
 
+
+
+// All Singles Area
+
 	public function AllSingles()
 	{
-		// $gender = $this->session->userdata('gender');
-		$AllSinglesCount = $this->Model_account->getAllSinglesCount();
+		$gender = $this->session->userdata('gender');
+		$member_id = $this->session->userdata('member_id');
 
-		$this->data['AllSinglesCount'] = $AllSinglesCount;
+		$AllSinglesCount = $this->Model_account->getAllSinglesCount();
+		$MyMatchesCount = $this->Model_account->getMyMatches($member_id, $gender);
+
+		$this->data['allSinglesCount'] = $AllSinglesCount;
+		$this->data['myMatchesCount'] = $MyMatchesCount;
 
 		$this->render_template('account/all_singles', 'All Singles',  $this->data);
+	}
+
+	public function LikeOrDisLikeProfile(){
+		$member_id = $this->session->userdata('member_id');
+		$MemberID_Partner =  $this->input->post('MemberID');
+		$gender = $this->session->userdata('gender'); 
+
+		$result = $this->Model_account->updateLikeOrDisLikeProfile($member_id, $MemberID_Partner, $gender);
+
+
+		// var_dump($result);
+
+		if ($result == true) {
+			$response['success'] = true;
+		} else {
+			$response['success'] = false;
+			$response['messages'] = 'Error in the database while like profile, Please contact service provider.';
+		}
+		
+		echo json_encode($response);
 	}
 
 	public function MatchProfile()
 	{
 
 		$member_id = $this->session->userdata('member_id');
-		// $user_id= 18;
 		$memberData = $this->Model_account->getMemberData($member_id);
 
 		$this->data['userData'] = $memberData;
@@ -44,12 +70,15 @@ class Account extends Admin_Controller{
 		$this->render_template('account/match_profile', 'Match Profile',  null);
 	}
 
-	public function GetMyMatches(){
+	public function GetMyMatches()
+	{
 		$member_id = $this->session->userdata('member_id');
 		$gender = $this->session->userdata('gender');
 
 		$result = $this->Model_account->getMyMatches($member_id, $gender);
 		echo json_encode($result);
 	}
-
 }
+
+// All Single Area - End
+
