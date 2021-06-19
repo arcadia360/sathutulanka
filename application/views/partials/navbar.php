90781d0440b2b953c9f417cde99d67e87127a313
Viraj <virajlasithazeero@gmail.com>
2021-06-20 00:18:33 +0530

    Update navbar.php

--- application/views/partials/navbar.php
+++ application/views/partials/navbar.php
@@ -7,6 +7,11 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Viraj Start -->
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=3">
+    <!-- Viraj End -->
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page_title ?></title>
     <!-- Font Awesome -->
