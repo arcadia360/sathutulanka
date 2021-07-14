2b846849a7fe8e040b63d2658181472c7bb1cac6
Ramodk <42975445+Ramodk@users.noreply.github.com>
2021-07-14 22:12:08 +0530

    Update si_lang.php

--- application/language/sinhala/si_lang.php
+++ application/language/sinhala/si_lang.php
@@ -1,10 +1,58 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+//navbar
+$lang['login'] = 'ඇතුල් වන්න';
+
+//Login
+$lang['welcome_to_login'] = 'පුරනය වීමේ පිවිසුම වෙත සාදරයෙන් පිළිගනිමු';
+$lang['email_phone'] = 'විද්‍යුත් තැපෑල / දුරකතන අංකය';
+$lang['password'] = 'මුර පදය';
+$lang['remember_me'] = 'මතක තබා ගන්න';
+$lang['forget_password'] = 'මුරපදය අමතකද?';
+$lang['sign_in'] = 'ඇතුලුවන්න';
+$lang['not_a_member'] = 'සාමාජිකයෙක් නොවේද?';
+$lang['register_free'] = 'නොමිලේ ලියාපදිංචි වන්න';
+
+//navbar
+$lang['login'] = 'ඇතුල් වන්න';
+
+$lang['select'] = 'තෝරන්න';
+$lang['please'] = 'කරුණාකර';
+$lang['noIdea'] = 'නොදැනුවත්ය';
+$lang['no'] = 'නැත';
+$lang['inDetails'] = 'විස්තර ලිඛිතව - MY Story';
+
+
 // Physical Status
 $lang['physicalStatus'] = 'භෞතික තත්වය';
 $lang['height'] = 'උස';
 $lang['weight'] = 'බර';
+$lang['bodyShape'] = 'ශරීර හැඩය';
+$lang['slim'] = 'සිහින්';
+$lang['average'] = 'සාමාන්‍යය';
+$lang['heavy'] = 'බර';
+$lang['skinColour'] = 'සමෙහි වර්ණය';
+$lang['veryFair'] = 'ඉතා සුදු';
+$lang['fair'] = 'සුදු';
+$lang['lightBrown'] = 'ලා දුඹුරු';
+$lang['darkBrown'] = 'තද දුඹුරු';
+$lang['blackBrown'] = 'කළු දුඹුරු';
+$lang['disability'] = 'ආබාධිතතාවයන්';
+$lang['bloodGroup'] = 'ලේ වර්ගය';
+$lang['healthInfo'] = 'සෞඛ්‍ය තොරතුරු';
+$lang['noDisability'] = 'ආබාධතා නැත';
+$lang['noHealthIssues'] = 'සෞඛ්‍ය ප්‍රශ්න නැත';
+$lang['heightErr'] = 'කරුණාකර උස තෝරන්න !';
+$lang['weightErr'] = 'කරුණාකර බර තෝරන්න !';
+$lang['bodyTypetErr'] = 'කරුණාකර ශරීර හැඩය තෝරන්න !';
+$lang['skinColorErr'] = 'කරුණාකර ශරීර වර්ණය තෝරන්න !';
+$lang['disabilityErr'] = 'කරුණාකර ආබාධිතතාවයන් තෝරන්න !';
+$lang['bloodGroupErr'] = 'කරුණාකර ලේ වර්ගය තෝරන්න !';
+$lang['healthInfoErr'] = 'කරුණාකර සෞඛ්‍ය තොරතුරු තෝරන්න !';
+
+
+
 
 
 // Background
@@ -15,3 +63,280 @@ $lang['religion'] = 'ආගම';
 $lang['caste'] = 'කුලය';
 $lang['subCast'] = 'උප කුලය';
 $lang['policeReport'] = 'පොලිස් වාර්තාව';
+
+
+//Registration
+$lang['male'] = 'පිරිමි';
+$lang['female'] = 'ගැහැණු';
+
+$lang['create_your_account'] = 'ඔබගේ ගිණුම හදන්න';
+$lang['short_name'] = 'කෙටි&nbsp;නම';
+$lang['country_code'] = 'දුරකතන&nbsp;කේතයන්';
+$lang['mobile_number'] = 'දුරකතන&nbsp;අංකය';
+$lang['new_password'] = 'නව&nbsp;මුරපදය';
+$lang['confirm_password'] = 'මුරපදය&nbsp;නැවතත්';
+$lang['email'] = 'විද්යුත්&nbsp;තැපෑල';
+
+$lang['never married'] = 'අවිවාහකයි';
+$lang['divorced'] = 'දික්කසාදයි';
+$lang['awaiting divorce'] = 'දික්කසාදය&nbsp;බලාපොරොත්තුවෙන්';
+$lang['separated (not legally)'] = 'වෙන්වූ&nbsp;(නීත්‍යානුකූල&nbsp;නොවේ)';
+$lang['widowed'] = 'වැන්දඹු';
+$lang['annulled'] = 'නීතියෙන්&nbsp;අවලංගු&nbsp;කල&nbsp;විවාහය';
+$lang['divorced only at engagement'] = 'අත්සනට&nbsp;සීමා&nbsp;වූ&nbsp;දික්කසාදය';
+$lang['virginity problem'] = 'කන්‍යාභාවය&nbsp;පිළිබඳ&nbsp;ගැටළුවකි';
+
+$lang['self'] = 'තමා&nbsp;විසින්';
+$lang['parents'] = 'දෙමව්පියන්&nbsp;විසින්';
+$lang['guardian'] = 'භාරකරු&nbsp;විසින්';
+$lang['relative'] = 'ඥාතියෙකු&nbsp;විසින්';
+$lang['brother'] = 'සහෝදරයා&nbsp;විසින්';
+$lang['sister'] = 'සහෝදරිය&nbsp;විසින්';
+$lang['friend'] = 'මිත්‍රයකු&nbsp;විසින්';
+$lang['mentioned in writing'] = 'ලිඛිතව&nbsp;ඇත';
+
+
+//DK
+
+$lang['dataCannotRetrieve'] = 'දත්ත ලබාගත නොහැක';
+
+//navbar
+$lang['login'] = 'ඇතුල් වන්න';
+
+//Login
+$lang['welcome_to_login'] = 'පුරනය වීමේ පිවිසුම වෙත සාදරයෙන් පිළිගනිමු';
+$lang['email_phone'] = 'විද්‍යුත් තැපෑල / දුරකතන අංකය';
+$lang['password'] = 'මුර පදය';
+$lang['remember_me'] = 'මතක තබා ගන්න';
+$lang['forget_password'] = 'මුරපදය අමතකද?';
+$lang['sign_in'] = 'ඇතුලුවන්න';
+$lang['not_a_member'] = 'සාමාජිකයෙක් නොවේද?';
+$lang['register_free'] = 'නොමිලේ ලියාපදිංචි වන්න';
+
+$lang['select'] = 'තෝරන්න';
+$lang['please'] = 'කරුණාකර';
+$lang['noIdea'] = 'නොදැනුවත්ය';
+$lang['no'] = 'නැත';
+$lang['yes'] = 'ඔව්';
+$lang['inDetails'] = 'විස්තර ලිඛිතව - MY Story';
+
+
+// Physical Status
+$lang['physicalStatus'] = 'භෞතික තත්වය';
+$lang['height'] = 'උස';
+$lang['weight'] = 'බර';
+$lang['weightScale'] = 'කිලෝග්රෑම්';
+$lang['bodyShape'] = 'ශරීර හැඩය';
+$lang['slim'] = 'සිහින්';
+$lang['average'] = 'සාමාන්‍යය';
+$lang['heavy'] = 'බර';
+$lang['skinColour'] = 'සමෙහි වර්ණය';
+$lang['veryFair'] = 'ඉතා සුදු';
+$lang['fair'] = 'සුදු';
+$lang['lightBrown'] = 'ලා දුඹුරු';
+$lang['darkBrown'] = 'තද දුඹුරු';
+$lang['blackBrown'] = 'කළු දුඹුරු';
+$lang['disability'] = 'ආබාධිතතාවයන්';
+$lang['bloodGroup'] = 'ලේ වර්ගය';
+$lang['healthInfo'] = 'සෞඛ්‍ය තොරතුරු';
+$lang['noDisability'] = 'ආබාධතා නැත';
+$lang['noHealthIssues'] = 'සෞඛ්‍ය ප්‍රශ්න නැත';
+$lang['heightErr'] = 'කරුණාකර උස තෝරන්න !';
+$lang['weightErr'] = 'කරුණාකර බර තෝරන්න !';
+$lang['bodyTypetErr'] = 'කරුණාකර ශරීර හැඩය තෝරන්න !';
+$lang['skinColorErr'] = 'කරුණාකර ශරීර වර්ණය තෝරන්න !';
+$lang['disabilityErr'] = 'කරුණාකර ආබාධිතතාවයන් තෝරන්න !';
+$lang['bloodGroupErr'] = 'කරුණාකර ලේ වර්ගය තෝරන්න !';
+$lang['healthInfoErr'] = 'කරුණාකර සෞඛ්‍ය තොරතුරු තෝරන්න !';
+
+//Residance
+$lang['residance'] = 'පදිංචිය';
+$lang['liveAnotherCountry'] = 'වෙනත් රටක ජීවත් වේ නම්';
+$lang['Country'] = 'රට';
+$lang['liveInSriLanka'] = 'ශ්‍රි ලංකාවේ ජීවත් වන්නේ';
+$lang['AddressofSriLanka'] = 'ශ්‍රි ලංකාවේ ලිපිනය';
+$lang['ownershipOfTheAddress'] = 'ලිපිනයේ හිමිකාරීත්වය';
+$lang['ownPlace'] = 'තමාගේම ස්ථානයක';
+$lang['notOwnPlace'] = 'තමන්ගේම ස්ථානයක් නොවේ';
+$lang['district'] = 'දිස්ත්‍රික්ක';
+$lang['nativeDistrict'] = 'උපන් දිස්ත්‍රික්කය';
+$lang['currentlyLiveIn'] = 'වර්තමානයේ ජීවත්වන්නේ';
+$lang['sriLanka'] = 'ශ්‍රී ලංකාව';
+$lang['overSeas'] = 'වෙනත් රටක';
+$lang['	CentralProvince']  =  'මධ්‍යහ පළාත';
+$lang['	EasternProvince']  =  'නැගෙනහිර පළාත';
+$lang['	NorthCentralProvince']  =  'උතුරු මැද පළාත';
+$lang['	NorthWesternProvince']  =  'වයඹ පළාත';
+$lang['	NothernProvince']  =  'උතුරු පළාත';
+$lang['	SabaragamuwaProvince']  =  'සබරගමුව පළාත';
+$lang['	SouthernProvince']  =  'දකුණු පළාත';
+$lang['	UvaProvince']  =  'ඌව පළාත';
+$lang['	WesternProvince']  =  'බස්නාහිර පළාත';
+
+
+
+
+
+// Background 
+$lang['city'] = 'නගර';
+$lang['background'] = 'පසුබිම';
+$lang['motherTongue'] = 'මවු භාෂාව';
+$lang['ethnicity'] = 'ජනවාර්ගිකත්වය';
+$lang['religion'] = 'ආගම';
+$lang['caste'] = 'කුලය';
+$lang['subCast'] = 'උප කුලය';
+$lang['policeReport'] = 'පොලිස් වාර්තාව';
+$lang['Sinhalese'] = 'සිංහල';
+$lang['sriLankanTamils'] = 'ශ්‍රී ලාංකික දෙමළ';
+$lang['muslims'] = 'මුස්ලිම්';
+$lang['indianTamils'] = 'ඉන්දීය දෙමළ';
+$lang['sriLankanMalays'] = 'ශ්‍රී ලාංකික මැලේ';
+$lang['burghers'] = 'බර්ගර්';
+$lang['eurasians'] = 'යුරේසියානු';
+$lang['mentionedInMyStory'] = 'My Story හි සදහන් කරනු ලැබේ';
+$lang['sinhala'] = 'සිංහල';
+$lang['tamil'] = 'දෙමළ';
+$lang['english'] = 'ඉංග්‍රීසි';
+$lang['other'] = 'වෙනත්';
+
+$lang['buddhist'] = 'බුද්ධාගම';
+$lang['hindu'] = 'හින්දු';
+$lang['islam'] = 'ඉස්ලාම්';
+$lang['christians'] = 'කතෝලික';
+$lang['romanCatholic'] = 'රෝමානු කතෝලික';
+$lang['canProvide'] = 'සැපයිය හැකිය';
+$lang['cantProvide'] = 'සැපයිය නොහැක';
+$lang['disregardTheCaste'] = 'කුලභේද නොසැලකේ';
+
+//life style
+$lang['lifeStyle'] = 'ජීවන රටාව';
+$lang['diet'] = 'ආහාර';
+$lang['veg'] = 'නිර්මාංශ';
+$lang['NonVeg'] = 'නිර්මාංශ නොවන';
+$lang['OccasionallyNonVeg'] = 'ඉඳහිට නිර්මාංශයි';
+$lang['drink'] = 'මත්පැන්';
+$lang['duringFunction'] = 'උත්සව අවස්ථාවකදී';
+$lang['stopped'] = 'නවතා ඇත';
+$lang['smoke'] = 'දුම්පානය';
+$lang['languagesSpeak'] = 'භාෂාවන්';
+$lang['dressAndMakeup'] = 'ඇදුම් සහ රූපලාවන්‍ය';
+$lang['traditional'] = 'පාරම්පරික';
+$lang['modern'] = 'නවීන';
+$lang['mixofTraditionalAndModern'] = 'පාරම්පරික සහ නවීන මිශ්‍ර';
+$lang['shortDresses'] = 'කෙටි ඇදුම්';
+$lang['usedToTravel'] = 'ප්‍රවාහන පුරුදු';
+$lang['public'] = 'පොදු';
+$lang['private'] = 'පෞද්ගලික';
+$lang['publicAndPrivate'] = 'පොදු සහ පෞද්ගලික';
+$lang['CallTtParents'] = 'දෙමව්පියන් ඇමතීම';
+$lang['CallTtParentsT'] = '(Thaththa/ Amma/ Appachchi)';
+$lang['CallTtParentsM'] = '(Daddy/ Mammy/ Uncle / Aunt)';
+$lang['customs'] = 'චාරිත්‍ර වාරිත්‍ර';
+$lang['respect'] = 'ගරු කරයි';
+$lang['littleRespect'] = 'සුලු ගෞරවයකි';
+$lang['noRespect'] = 'ගෞරවයක් නැත';
+$lang['LiveIn'] = 'ජීවත් වන්නේ';
+$lang['normalHouse'] = 'සාමාන්‍ය නිවසකි';
+$lang['semiLuxuryHouse'] = 'අර්ධ සුඛෝපබෝගි නිවසකි';
+$lang['luxuryHouse'] = 'සුඛෝපබෝගි නිවසකි';
+
+
+//Districts
+$lang['Kandy']  =  'මහනුවර';
+$lang['Matale']  =  'මාතලේ';
+$lang['NuwaraEliya']  =  'නුවරඑළිය';
+$lang['Ampara']  =  'අම්පාර';
+$lang['Batticaloa']  =  'මඩකලපුව';
+$lang['Trincomalee']  =  'ත්රිකුණාමලය';
+$lang['Anuradhapura']  =  'අනුරාධපුර';
+$lang['Polonnaruwa']  =  'පොළොන්නරුව';
+$lang['Kurunegala']  =  'කුරුණෑගල';
+$lang['Puttalam']  =  'පුත්තලම';
+$lang['Jaffna']  =  'යාපනය';
+$lang['Kilinochchi']  =  'කිලිනොච්චි';
+$lang['Mannar']  =  'මන්නාරම';
+$lang['Mullaitivu']  =  'මුලතිව්';
+$lang['Vavuniya']  =  'වවුනියාව';
+$lang['Kegalle']  =  'කෑගල්ල';
+$lang['Ratnapura']  =  'රත්නපුර';
+$lang['Galle']  =  'ගාල්ල';
+$lang['Hambantota']  =  'හම්බන්තොට';
+$lang['Matara']  =  'මාතර';
+$lang['Badulla']  =  'බදුල්ල';
+$lang['Monaragala']  =  'මොණරගල';
+$lang['Colombo']  =  'කොළඹ';
+$lang['Gampaha']  =  'ගම්පහ';
+$lang['Kalutara']  =  'කළුතර';
+
+//city
+$lang['Colombo01'] = 'කොළඹ 01';
+$lang['Colombo02'] = 'කොළඹ 02';
+$lang['Colombo03'] = 'කොළඹ 03';
+$lang['Colombo04'] = 'කොළඹ 04';
+$lang['Colombo05'] = 'කොළඹ 05';
+$lang['Colombo06'] = 'කොළඹ 06';
+$lang['Colombo07'] = 'කොළඹ 07';
+$lang['Colombo08'] = 'කොළඹ 08';
+$lang['Colombo09'] = 'කොළඹ 09';
+$lang['Colombo10'] = 'කොළඹ 10';
+$lang['Colombo11'] = 'කොළඹ 11';
+$lang['Colombo12'] = 'කොළඹ 12';
+$lang['Colombo13'] = 'කොළඹ 13';
+$lang['Colombo14'] = 'කොළඹ 14';
+$lang['Colombo15'] = 'කොළඹ 15';
+$lang['Angoda'] = 'අංගොඩ';
+$lang['Athurugiriya'] = 'අතුරුගිරිය';
+$lang['Avissawella'] = 'අවිස්සාවේල්ල';
+$lang['Battaramulla'] = 'බත්තරමුල්ල';
+$lang['Boralesgamuwa'] = 'බොරලැස්ගමුව';
+$lang['Dehiwala'] = 'දෙහිවල';
+$lang['Hanwella'] = 'හංවැල්ල';
+$lang['Homagama'] = 'හෝමාගම';
+$lang['Kaduwela'] = 'කඩුවෙල';
+$lang['Kesbewa'] = 'කැස්බෑව';
+$lang['Kolonnawa'] = 'කොලොන්නාව';
+$lang['Kohuwala'] = 'කොහුවල';
+$lang['Kottawa'] = 'කොට්ටාව';
+$lang['Kotte'] = 'කෝට්ටේ';
+$lang['Maharagama'] = 'මහරගම';
+$lang['Malabe'] = 'මාලඹේ';
+$lang['Moratuwa'] = 'මොරටුව';
+$lang['MtLavinia'] = 'ගල්කිස්ස';
+$lang['Nawala'] = 'නාවල';
+$lang['Nugegoda'] = 'නුගේගොඩ';
+$lang['Padukka'] = 'පාදුක්ක';
+$lang['Pannipitiya'] = 'පන්නිපිටිය';
+$lang['Piliyandala'] = 'පිළියන්දල';
+$lang['Rajagiriya'] = 'රාජගිරිය';
+$lang['Ratmalana'] = 'රත්මලාන';
+$lang['Wellampitiya'] = 'වැල්ලම්පිටිය';
+$lang['Thalawatugoda'] = 'තලවතුගොඩ';
+$lang['Aluthgama'] = 'අලුත්ගම';
+$lang['Bandaragama'] = 'බණ්ඩාරගම';
+$lang['Beruwala'] = 'බේරුවල';
+$lang['Horana'] = 'හොරණ';
+$lang['Ingiriya'] = 'ඉංගිරිය';
+$lang['Kaluthara'] = 'කළුතර';
+$lang['Matugama'] = 'මතුගම';
+$lang['Panadura'] = 'පානදුර';
+$lang['Wadduwa'] = 'වාද්දුව';
+$lang['Delgoda'] = 'දෙල්ගොඩ';
+$lang['Divulapitiya'] = 'දිවුලපිටිය';
+$lang['Gampaha'] = 'ගම්පහ';
+$lang['Ganemulla'] = 'ගනේමුල්ල';
+$lang['Ja-ela'] = 'ජා ඇල';
+$lang['Kadawatha'] = 'කඩවත';
+$lang['Kandana'] = 'කදාන';
+$lang['Katunayaka'] = 'කටුනායක';
+$lang['Kelaniya'] = 'කැලණිය';
+$lang['Kiribathgoda'] = 'කිරිබත්ගොඩ';
+$lang['Minuwangoda'] = 'මිනුවන්ගොඩ';
+$lang['Mirigama'] = 'මීරිගම';
+$lang['Negombo'] = 'මීගමුව';
+$lang['Nittambuwa'] = 'නිට්ටඹුව';
+$lang['Ragama'] = 'රාගම';
+$lang['Veyangoda'] = 'වේයන්ගොඩ';
+$lang['Wattala'] = 'වත්තල';
+
+
+//END DK 
