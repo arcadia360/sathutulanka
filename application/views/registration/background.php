049d9c2ee5d28d1610d1a7ad63ca5cac3d57e756
Dhanushka Kumanayake <48651757+dhanu159@users.noreply.github.com>
2021-07-17 22:19:01 +0530

    Update background.php

--- application/views/registration/background.php
+++ application/views/registration/background.php
@@ -40,7 +40,7 @@
             <?php } ?>
           </select>
         </div>
-      </div>     
+      </div>
     </div>
 
     <div class="row">
@@ -100,165 +100,17 @@
 </div>
 
 <script>
-  $(function() {
+  var Member = function() {
+    this.MemberID = 0;
+  }
+  var model = new Member();
+  model.MemberID = (<?= $this->session->userdata('member_id') ?>);
 
-    var Member = function() {
-      this.MemberID = 0;
-    }
-    var model = new Member();
-    model.MemberID = (<?= $this->session->userdata('member_id') ?>);
-
-    ajaxCall('registration/getMemberData', model, function(response) {
-      if (response.intMotherTongueID != null) {
-        $("#motherTongue").val(response.intMotherTongueID);
-        $("#ethnicity").val(response.intEthnicityID);
-        $('#religion').val(response.intReligionID);
-        $("#caste").val(response.intCasteID);
-        $('#caste').trigger('change');
-        $('#subCaste').val(response.intSubCasteId);
-        $('#subCaste').trigger('change');
-        isPoliceReportCanProvide = (response.isPoliceReportCanProvide);
-        if (isPoliceReportCanProvide == 1) {
-          document.getElementById("policeReport1").checked = true;
-        } else {
-          document.getElementById("policeReport2").checked = true;
-        }
-      }
-
-
-    });
-
-
-
-    loadCaste();
-    $('#btnSubmit').click(function() {
-      var policeReport = $("input[name=policeReport]").is(":checked");
-      if ($('#motherTongue').val() == 0) {
-        toastr["error"]("Please select mother tounge !");
-        $("#motherTongue").focus();
-      } else if ($('#ethnicity').val() == 0) {
-        toastr["error"]("Please select ethnicity !");
-        $("#ethnicity").focus();
-      } else if ($('#religion').val() == 0) {
-        toastr["error"]("Please select religion !");
-        $("#religion").focus();
-      } else if ($('#caste').val() == 0) {
-        toastr["error"]("Please select caste !");
-        $("#caste").focus();
-      } else if (($('#caste').val() != 0 && $('#caste').val() != 2 && $('#caste').val() != 1) && $('#subCaste').val() == 0) {
-        toastr["error"]("Please select sub caste !");
-        $("#subCaste").focus();
-      } else if (!policeReport) {
-        toastr["error"]("Please select police report !");
-      } else {
-        var form = $("#addBackgroundDetails");
-        $.ajax({
-          type: form.attr('method'),
-          url: form.attr('action'),
-          data: form.serialize(),
-          dataType: 'json',
-          success: function(response) {
-            if (response.success == true) {
-              Swal.fire({
-                icon: 'success',
-                title: 'Background details saved successfully!',
-                showConfirmButton: false,
-                timer: 2000
-              }).then((result) => {
-                if (result.dismiss === Swal.DismissReason.timer) {
-                  window.location.href = "<?= base_url('Registration/lifeStyle') ?>";
-                }
-              })
-            } else {
-              if (response.messages instanceof Object) {
-                $.each(response.messages, function(index, value) {
-                  var id = $("#" + index);
-                  id.closest('.form-group')
-                    .removeClass('has-error')
-                    .removeClass('has-success')
-                    .addClass(value.length > 0 ? 'has-error' : 'has-success');
-                  id.after(value);
-                });
-              } else {
-                toastr["error"](response.messages);
-                $(button).prop('disabled', false);
-              }
-            }
-          }
-        });
-      }
-    });
-
-    function loadCaste() {
-      $.ajax({
-        type: 'ajax',
-        url: '<?php echo base_url(); ?>Registration/loadCaste',
-        async: false,
-        dataType: 'json',
-        success: function(data) {
-          if (!data) {
-            toastr["error"]("<?= 'Caste ' . lang('dataCannotRetrieve') ?>");
-          } else {
-            $('#caste').html(data);;
-            $('#caste').val(0);
-          }
-        },
-        error: function() {
-          toastr["error"]("<?= 'Caste ' . lang('dataCannotRetrieve') . ' Connection error' ?>");
-        }
-      });
-    }
-
-    //load sub caste 
-    $('#caste').change(function() {
-      if (($('#caste').val() != 0 && $('#caste').val() != 1 && $('#caste').val() != 2 && $('#caste').val() != 3)) {
-        var casteID = $('#caste').val()
-        $.ajax({
-          type: 'POST',
-          url: '<?php echo base_url(); ?>Registration/loadSubCaste',
-          async: false,
-          dataType: 'json',
-          data: {
-            'casteID': casteID
-          },
-          success: function(data) {
-            if (!data) {
-              toastr["error"]("<?= 'Caste ' . lang('dataCannotRetrieve') ?>");
-            } else {
-              $('#subCaste').html(data);;
-              $('#subCaste').val(0);
-              $('#sub-caste-div').show();
-            }
-          },
-          error: function() {
-            toastr["error"]("internal error Failed to load sub caste selection data");
-          }
-        });
-      } else {
-        $('#sub-caste-div').hide();
-        $('#subCaste').val(0);
-      }
-    });
-
-    $('#sub-caste-div').hide();
-
-    $('#btnBack').click(function() {
-      window.location.href = "<?php echo base_url('Registration/residence') ?>";
-    });
-
-    // $('#btnSubmit').click(function() {
-    // window.location.href = "<?php echo base_url('Registration/lifeStyle') ?>";
-    // });
-
-    // $('#caste').change(function() {
-    //   var caste = $('#caste').val();
-    //   console.log(caste);
-    //   if (caste == 0 || caste == 1 || caste == 2) {
-    //     $('#sub-caste-div').hide();
-    //   } else {
-    //     $('#sub-caste-div').show();
-    //   }
-    // });
-
-  });
+  //
+  var loadDisabilityUrl = '<?php echo base_url(); ?>Registration/loadDisabilityDetails';
+  let navigateTo = "<?= base_url('Registration/lifeStyle') ?>";
+  let loadCasteUrl = '<?php echo base_url(); ?>Registration/loadCaste';
+  let loadSubCasteUrl = '<?php echo base_url(); ?>Registration/loadSubCaste';
+  let navigateBack = "<?php echo base_url('Registration/residence') ?>";
 </script>
+<script src="<?= base_url('resources/js/registration_form/background.js') ?>"></script>
