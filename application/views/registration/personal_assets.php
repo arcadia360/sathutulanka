2c4c772964c82667b4096605590759c6ab6030a2
Dhanushka Kumanayake <48651757+dhanu159@users.noreply.github.com>
2021-07-24 19:45:57 +0530

    Update personal_assets.php

--- application/views/registration/personal_assets.php
+++ application/views/registration/personal_assets.php
@@ -1,6 +1,6 @@
 <div class="offset-lg-3 col-lg-6 main-section">
     <h3 class="text-center text-inverse title">Personal Assets</h3>
-    <span class="highLightText">Will not publish, Office purpose only.</span>
+    <span class="highLightText">100% Privacy Controls Available</span>
     <form method="post" action="<?= base_url('Registration/addPersonalAssestDetails') ?>" id="addPersonalAssestDetails">
         <div class="row">
             <div class="col-lg-12 col-sm-12 col-12">
@@ -23,27 +23,33 @@
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language routeIncome" id="incomeRoute1" name="incomeRoute[]" value="Job">
-                                <br>
-                                <span class="custom-control-description">Job</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language routeIncome" id="incomeRoute1" name="incomeRoute[]" value="Job">
+                                    <label for="incomeRoute1"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Job</span></div>
                             </center>
                         </div>
                     </div>
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language routeIncome" id="incomeRoute2" name="incomeRoute[]" value="Rent/Lease">
-                                <br>
-                                <span class="custom-control-description">Rent/Lease</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language routeIncome" id="incomeRoute2" name="incomeRoute[]" value="Rent/Lease">
+                                    <label for="incomeRoute2"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Rent/Lease</span></div>
                             </center>
                         </div>
                     </div>
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language routeIncome" id="incomeRoute3" name="incomeRoute[]" value="Fixed Deposit">
-                                <br>
-                                <span class="custom-control-description">Fixed Deposit</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language routeIncome" id="incomeRoute3" name="incomeRoute[]" value="Fixed Deposit">
+                                    <label for="incomeRoute3"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Fixed Deposit</span></div>
                             </center>
                         </div>
                     </div>
@@ -56,27 +62,33 @@
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language routeIncome" id="incomeRoute4" name="incomeRoute[]" value="Shares">
-                                <br>
-                                <span class="custom-control-description">Shares</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language routeIncome" id="incomeRoute4" name="incomeRoute[]" value="Shares">
+                                    <label for="incomeRoute4"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Shares</span></div>
                             </center>
                         </div>
                     </div>
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language routeIncome" id="incomeRoute5" name="incomeRoute[]" value="Business">
-                                <br>
-                                <span class="custom-control-description">Business</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language routeIncome" id="incomeRoute5" name="incomeRoute[]" value="Business">
+                                    <label for="incomeRoute5"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Business</span></div>
                             </center>
                         </div>
                     </div>
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language routeIncome" id="incomeRoute6" name="incomeRoute[]" value="Ask (My Story)">
-                                <br>
-                                <span class="custom-control-description">Ask (My Story)</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language routeIncome" id="incomeRoute6" name="incomeRoute[]" value="Details in writing">
+                                    <label for="incomeRoute6"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Details in writing</span></div>
                             </center>
                         </div>
                     </div>
@@ -105,27 +117,33 @@
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language assetRoutes" id="assestRoute1" name="assestRoute[]" value="Land">
-                                <br>
-                                <span class="custom-control-description">Land</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language assetRoutes" id="assestRoute1" name="assestRoute[]" value="Land">
+                                    <label for="assestRoute1"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Land</span></div>
                             </center>
                         </div>
                     </div>
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language assetRoutes" id="assestRoute2" name="assestRoute[]" value="Vehicles">
-                                <br>
-                                <span class="custom-control-description">Vehicles</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language assetRoutes" id="assestRoute2" name="assestRoute[]" value="Vehicles">
+                                    <label for="assestRoute2"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Vehicles</span></div>
                             </center>
                         </div>
                     </div>
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language assetRoutes" id="assestRoute3" name="assestRoute[]" value="House">
-                                <br>
-                                <span class="custom-control-description">House</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language assetRoutes" id="assestRoute3" name="assestRoute[]" value="House">
+                                    <label for="assestRoute3"></label>
+                                </div>
+                                <div> <span class="custom-control-description">House</span></div>
                             </center>
                         </div>
                     </div>
@@ -138,27 +156,33 @@
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language assetRoutes" id="assestRoute4" name="assestRoute[]" value="Business">
-                                <br>
-                                <span class="custom-control-description">Business</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language assetRoutes" id="assestRoute4" name="assestRoute[]" value="Business">
+                                    <label for="assestRoute4"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Business</span></div>
                             </center>
                         </div>
                     </div>
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language assetRoutes" id="assestRoute5" name="assestRoute[]" value="Jewellery">
-                                <br>
-                                <span class="custom-control-description">Jewellery</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language assetRoutes" id="assestRoute5" name="assestRoute[]" value="Jewellery">
+                                    <label for="assestRoute5"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Jewellery</span></div>
                             </center>
                         </div>
                     </div>
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language assetRoutes" id="assestRoute6" name="assestRoute[]" value="Savings">
-                                <br>
-                                <span class="custom-control-description">Savings</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language assetRoutes" id="assestRoute6" name="assestRoute[]" value="Savings">
+                                    <label for="assestRoute6"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Savings</span></div>
                             </center>
                         </div>
                     </div>
@@ -172,18 +196,22 @@
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language assetRoutes" id="assestRoute7" name="assestRoute[]" value="Business Premises">
-                                <br>
-                                <span class="custom-control-description">Business Premises</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language assetRoutes" id="assestRoute7" name="assestRoute[]" value="Business Premises">
+                                    <label for="assestRoute7"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Business Premises</span></div>
                             </center>
                         </div>
                     </div>
                     <div class="col-4">
                         <div class="form-group">
                             <center>
-                                <input type="checkbox" class="language assetRoutes" id="assestRoute8" name="assestRoute[]" value="Ask (My Story)">
-                                <br>
-                                <span class="custom-control-description">Ask (My Story)</span>
+                                <div class="checkbox icheck-wetasphalt icheck-align">
+                                    <input type="checkbox" class="language assetRoutes" id="assestRoute8" name="assestRoute[]" value="Details in writing">
+                                    <label for="assestRoute8"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Details in writing</span></div>
                             </center>
                         </div>
                     </div>
@@ -201,30 +229,38 @@
                     <div class="row">
                         <div class="col-3">
                             <center>
-                                <input name="OwnershipOfAssets" id="OwnershipOfAssets1" type="radio" value="Parent">
-                                <br>
-                                <span class="custom-control-description">Parent</span>
+                                <div class="radio icheck-midnightblue icheck-align mx-auto">
+                                    <input name="OwnershipOfAssets" id="OwnershipOfAssets1" type="radio" value="Parent">
+                                    <label for="OwnershipOfAssets1"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Parent</span></div>
                             </center>
                         </div>
                         <div class="col-3">
                             <center>
-                                <input name="OwnershipOfAssets" id="OwnershipOfAssets2" type="radio" value="Self">
-                                <br>
-                                <span class="custom-control-description">Self</span>
+                                <div class="radio icheck-midnightblue icheck-align mx-auto">
+                                    <input name="OwnershipOfAssets" id="OwnershipOfAssets2" type="radio" value="Self">
+                                    <label for="OwnershipOfAssets2"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Self</span></div>
                             </center>
                         </div>
                         <div class="col-3">
                             <center>
-                                <input name="OwnershipOfAssets" id="OwnershipOfAssets3" type="radio" value="Under Parent and me">
-                                <br>
-                                <span class="custom-control-description">Under Parent and me</span>
+                                <div class="radio icheck-midnightblue icheck-align mx-auto">
+                                    <input name="OwnershipOfAssets" id="OwnershipOfAssets3" type="radio" value="Under Parent and me">
+                                    <label for="OwnershipOfAssets3"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Under Parent and me</span></div>
                             </center>
                         </div>
                         <div class="col-3">
                             <center>
-                                <input name="OwnershipOfAssets" id="OwnershipOfAssets4" type="radio" value="Ask (My Story)">
-                                <br>
-                                <span class="custom-control-description">Ask (My Story)</span>
+                                <div class="radio icheck-midnightblue icheck-align mx-auto">
+                                    <input name="OwnershipOfAssets" id="OwnershipOfAssets4" type="radio" value="Details in writing">
+                                    <label for="OwnershipOfAssets4"></label>
+                                </div>
+                                <div> <span class="custom-control-description">Details in writing</span></div>
                             </center>
                         </div>
                     </div>
