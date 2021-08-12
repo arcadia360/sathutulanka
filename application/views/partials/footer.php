1cf99b4cb399fc14d15328e0ab915b29ccea4cfb
Viraj <virajlasithazeero@gmail.com>
2021-08-12 23:54:20 +0530

    Update footer.php

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,29 +1,29 @@
 		<footer class="footer-distributed">
 
-		    <div class="footer-right">
+			<div class="footer-right">
 
-		        <a href="#"><i class="fab fa-facebook-f"></i></a>
-		        <a href="https://wa.me/+94777292932" target="new"><i class="fab fa-whatsapp"></i></a>
-		        <a href="tel:0115735736"><i class="fas fa-phone-alt"></i></a>
+				<a href="#"><i class="fab fa-facebook-f"></i></a>
+				<a href="https://wa.me/+94777292932" target="new"><i class="fab fa-whatsapp"></i></a>
+				<a href="tel:0115735736"><i class="fas fa-phone-alt"></i></a>
 
-		    </div>
+			</div>
 
-		    <div class="footer-left">
+			<div class="footer-left">
 
-		        <p class="footer-links">
+				<p class="footer-links">
 
-		            <a href="#">HELP & SUPPORT</a>
+					<a href="#" style="text-transform: uppercase;"><?= lang('help_support'); ?></a>
 
-		            <a href="#">ABOUT US</a>
+					<a href="#" style="text-transform: uppercase;"><?= lang('about_us'); ?></a>
 
-		            <a href="<?= base_url("Welcome/ContactUs")?>">CONTACT US</a>
-		        </p>
-		        <div class="col-md-12 px-0 pt-2 pb-3">
-		            <img src="<?= base_url("resources/images/sathutu-lanka-footer-logo.png") ?>" width="180px" alt="">
-		            <span class="">&nbsp; IS A REGISTERED TRADEMARK AND SHOULD NOT BE USED WITHOUT PERMISSION. ALL RIGHTS RESERVED.</span> 
-		        </div> 
-		        <p>Sathutulanka.lk &copy; 2021</p>
-		    </div>
+					<a href="<?= base_url("Welcome/ContactUs") ?>" style="text-transform: uppercase;"><?= lang('contact_us'); ?></a>
+				</p>
+				<div class="col-md-12 px-0 pt-2 pb-3">
+					<img src="<?= base_url("resources/images/sathutu-lanka-footer-logo.png") ?>" width="180px" alt="">
+					<span class="">&nbsp; <?= lang('footer_title'); ?></span>
+				</div>
+				<p>Sathutulanka.lk &copy; 2021</p>
+			</div>
 
 		</footer>
 
@@ -52,62 +52,62 @@
 		<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
 		<script>
-		    AOS.init({
-		        easing: 'ease-in-out-sine'
-		    });
+			AOS.init({
+				easing: 'ease-in-out-sine'
+			});
 
-		    toastr.options = {
-		        "closeButton": false,
-		        "debug": false,
-		        "newestOnTop": false,
-		        "progressBar": false,
-		        "positionClass": "toast-top-right",
-		        "preventDuplicates": false,
-		        "onclick": null,
-		        "showDuration": "300",
-		        "hideDuration": "1000",
-		        "timeOut": "500000",
-		        "extendedTimeOut": "1000",
-		        "showEasing": "swing",
-		        "hideEasing": "linear",
-		        "showMethod": "fadeIn",
-		        "hideMethod": "fadeOut"
-		    }
+			toastr.options = {
+				"closeButton": false,
+				"debug": false,
+				"newestOnTop": false,
+				"progressBar": false,
+				"positionClass": "toast-top-right",
+				"preventDuplicates": false,
+				"onclick": null,
+				"showDuration": "300",
+				"hideDuration": "1000",
+				"timeOut": "500000",
+				"extendedTimeOut": "1000",
+				"showEasing": "swing",
+				"hideEasing": "linear",
+				"showMethod": "fadeIn",
+				"hideMethod": "fadeOut"
+			}
 		</script>
 
 		<script type="text/javascript">
-		    $(document).ready(function() {
-		        // alert("dd");
-		        <?php if ($this->session->flashdata('errors')) { ?>
-		            toastr["error"]("<?= $this->session->flashdata('errors') ?>");
-		            // alert("Please fill in all fields !"); 
-		        <?php
-                } ?>
-		        // toastr["error"]("Please fill in all fields !");
-		    });
+			$(document).ready(function() {
+				// alert("dd");
+				<?php if ($this->session->flashdata('errors')) { ?>
+					toastr["error"]("<?= $this->session->flashdata('errors') ?>");
+					// alert("Please fill in all fields !"); 
+				<?php
+				} ?>
+				// toastr["error"]("Please fill in all fields !");
+			});
 		</script>
 
 
 		<script>
-		    // $(document).ready(function() {
+			// $(document).ready(function() {
 
-		    //     var span = document.getElementsByClassName("closeViewer")[0];
+			//     var span = document.getElementsByClassName("closeViewer")[0];
 
-		    //     span.onclick = function() {
-		    //         modal.style.display = "none";
-		    //     }
-		    // });
-		    // var modal = document.getElementById("myModal");
+			//     span.onclick = function() {
+			//         modal.style.display = "none";
+			//     }
+			// });
+			// var modal = document.getElementById("myModal");
 
-		    // var img = document.getElementById("myImg");
-		    // var modalImg = document.getElementById("img01");
-		    // // var captionText = document.getElementById("caption");
+			// var img = document.getElementById("myImg");
+			// var modalImg = document.getElementById("img01");
+			// // var captionText = document.getElementById("caption");
 
-		    // img.onclick = function() {
-		    //     modal.style.display = "block";
-		    //     modalImg.src = this.src;
-		    //     // captionText.innerHTML = this.alt;
-		    // }
+			// img.onclick = function() {
+			//     modal.style.display = "block";
+			//     modalImg.src = this.src;
+			//     // captionText.innerHTML = this.alt;
+			// }
 		</script>
 
 
