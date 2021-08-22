0dcbadd3add4f534706531024cdc6e6105be93bd
Viraj <virajlasithazeero@gmail.com>
2021-08-22 22:36:13 +0530

    Update login.js

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,8 +1,16 @@
 $(function () {
+    $('#varifyyouremail').hide();
+    $('#otpverifypending').hide();
+
     $("#icon-wait").hide();
     $("#invalid-email").hide();
+    $("#empty-email").hide();
     $("#invalid-password").hide();
-    $("#invalid-messages").hide();
+    $("#empty-password").hide();
+    $("#invalid-messages").hide(); 
+    $("#deactivated-messages").hide();
+  
+
 
     $("#show_hide_password a").on('click', function (event) {
         event.preventDefault();
@@ -25,21 +33,27 @@ $("#btn-signin").click(function () {
     // $('body').addClass("wait"); // Change Cursor To Progress
 
     $("#invalid-email").hide();
+    $("#empty-email").hide();
     $("#invalid-password").hide();
+    $("#empty-password").hide();
     $("#invalid-messages").hide();
+    $('#varifyyouremail').hide();
+    $('#otpverifypending').hide();
+
+
 
 
     if ($("#username").val().trim() === "") {
-        $("#invalid-email").text("Please Enter Your Email Address !");
-        $("#invalid-email").show();
+        // $("#empty-email").text("Please Enter Your Email Address !");
+        $("#empty-email").show();
     } else if (!validEmail($("#username").val().trim())) {
-        $("#invalid-email").text("Please Enter Valid Email Address !");
+        // $("#invalid-email").text("Please Enter a Valid Email Address !");
         $("#invalid-email").show();
         return;
     }
     if ($("#password").val().trim() === "") {
-        $("#invalid-password").text("Please Enter Your Password !");
-        $("#invalid-password").show();
+        // $("#empty-password").text("Please Enter Your Password !");
+        $("#empty-password").show();
     }
 
     if ($("#username").val().trim() !== "" && $("#password").val().trim() !== "") {
@@ -56,12 +70,18 @@ $("#btn-signin").click(function () {
                 // $("#icon-wait").hide();
             },
             success: function (response) {
-
                 // $('input[name=csrf_arcadia360]').val(response.token);
 
                 if (response.success == false) {
-                    $("#invalid-messages").text(response.messages);
-                    $("#invalid-messages").show();
+                    if (response.messages == 1) {
+                        $('#varifyyouremail').show();
+                    } else if (response.messages == 2) {
+                        $('#otpverifypending').show();
+                    } else if (response.messages == 3) {
+                        $("#invalid-messages").show();
+                    } else if (response.messages == 4) {
+                        $("#deactivated-messages").show();
+                    } 
                     $("#icon-wait").hide();
                 } else {
                     $("#icon-wait").hide();
