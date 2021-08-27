1057e40ed5ac279093bfe1fe80b98dea827dfdfc
Dhanushka Kumanayake <48651757+dhanu159@users.noreply.github.com>
2021-08-27 10:55:06 +0530

    Update visited_profile.php

--- application/views/account/visited_profile.php
+++ application/views/account/visited_profile.php
@@ -29,10 +29,41 @@
               <button class="btn btn-sm btn-light col-md-3">Blocked Profiles <span class="float-right uk-badge">1</span></button>
           </div>
       </div>
-      <div class="row" id="mini-profile-area">
 
+  </div>
+  <div class="container p-3">
+      <p class="display-5 text-center font-weight-bold">Visited Profile</p>
+      <nav>
+          <div class="nav nav-tabs" id="nav-tab" role="tablist">
+              <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Profiles Visited By Me</a>
+              <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile Visited By Others</a>
+          </div>
+      </nav>
+      <div class="tab-content" id="nav-tabContent">
+          <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+              <div class="row" id="mini-profile-area">
+              </div>
+          </div>
+          <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime nobis provident quae voluptatem a necessitatibus dolor corrupti cupiditate, asperiores, minus veritatis qui deleniti alias magni rem inventore, voluptas soluta consequuntur?</p>
 
+          </div>
       </div>
   </div>
+  <script>
+      $('#visitedByMe').click(function() {
+          $("#visitedByMe").addClass("active");
+          $("#visitedByOthers").removeClass("active");
+          $("#visitedByMeContent").addClass("show active");
+          $("#visitedByOthersContent").removeClass("show active");
+      });
+      $('#visitedByOthers').click(function() {
+          $("#visitedByOthers").addClass("active");
+          $("#visitedByMe").removeClass("active");
+          $("#visitedByOthersContent").addClass("show active");
+          $("#visitedByMeContent").removeClass("show active");
+      });
+  </script>
+
 
   <script src="<?= base_url("resources/js/pageJS/visitedProfiles.js") ?>"></script>
