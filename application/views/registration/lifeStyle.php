e0833e0024a72e34d6b2461206b4e127c4a130a0
Dhanushka Kumanayake <48651757+dhanu159@users.noreply.github.com>
2021-08-27 10:55:21 +0530

    Update lifeStyle.php

--- application/views/registration/lifeStyle.php
+++ application/views/registration/lifeStyle.php
@@ -344,7 +344,7 @@
     <div class="row">
       <div class="col-12">
         <button class="btn btn-info" id="btnBack" type="button"><i class="fas fa-angle-double-left"></i> &nbsp; BACK</button>
-        <button class="btn btn-info float-right" type="button" id="btnSubmit">Next &nbsp; <i class="fas fa-angle-double-right"></i><i class="" id="btnSubmitLoading"></i></button>
+        <button class="btn btn-info float-right" type="button" id="btnSubmit">Next &nbsp; <i class="fas fa-angle-double-right"></i>&nbsp; <i class="" id="btnSubmitLoading"></i></button>
       </div>
     </div>
   </form>
