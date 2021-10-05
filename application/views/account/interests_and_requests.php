c82dc29d96c4ad0d21bae25787e99a89c965f16b
Dhanushka Kumanayake <48651757+dhanu159@users.noreply.github.com>
2021-10-05 16:27:46 +0530

    Update interests_and_requests.php

--- application/views/account/interests_and_requests.php
+++ application/views/account/interests_and_requests.php
@@ -1,1 +1,13 @@
-hiii
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Ineterest <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Request</a>
+      <a class="nav-item nav-link" href="#">Pricing</a>
+      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+    </div>
+  </div>
+</nav>
